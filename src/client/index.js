906a0d8198598a5cd0435fd005347c296236457b
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-20 15:48:53 +0200

    Fix syntax following ESLint rules

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./components/app";
 import "./css/styles.css";
 
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+const mountNode = document.querySelector("#app");
+ReactDOM.render(<App />, mountNode);
