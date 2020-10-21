593738dafab3a2fa9a79714cfe99b870aa9d042c
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-21 14:59:55 +0200

    Add CSS classes to HTML tags

--- src/client/components/main-content.js
+++ src/client/components/main-content.js
@@ -4,8 +4,8 @@ import {hot} from "react-hot-loader/root";
 class Main extends React.Component {
     render() {
         return (
-            <div>
-                <h1>{"ARNAUD"}<br />{"DE BAERDEMAEKER"}</h1>
+            <div className={"title"}>
+                <h1 className={"title__main-title"}>{"ARNAUD"}<br />{"DE BAERDEMAEKER"}</h1>
             </div>
         );
     }
