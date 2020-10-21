321bb1c5f2ec870a293a1c0aa5817429f1b7332a
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-21 11:25:22 +0200

    Call the Header component in the app

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,17 +1,11 @@
+/* eslint-disable class-methods-use-this */
 import React from "react";
 import {hot} from "react-hot-loader/root";
+import Header from "./header";
 
 class App extends React.Component {
     render() {
-        const {name} = this.props;
-        return (
-            <>
-                <h1>
-                    {"Hello"}
-                    {name}
-                </h1>
-            </>
-        );
+        return <Header />;
     }
 }
 
