d9ab80f313f9c2bb7c4caf255e9ecd19e1efd32b
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-21 11:49:07 +0200

    Create the Main component

---
+++ src/client/components/main-content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {hot} from "react-hot-loader/root";
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{"ARNAUD"}<br />{"DE BAERDEMAEKER"}</h1>
+            </div>
+        );
+    }
+}
+
+export default hot(Main);
