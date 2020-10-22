7312280a67cd42476d7bc224494605098198c40c
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-22 16:41:52 +0200

    Add the about component
    Added the route to redirect to the about page.

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,8 +1,9 @@
 /* eslint-disable class-methods-use-this */
 import React from "react";
 import {hot} from "react-hot-loader/root";
-import Homepage from "./homepage.js";
 import {BrowserRouter, Route} from "react-router-dom";
+import Homepage from "./homepage";
+import About from "./about";
 
 class App extends React.Component {
     render() {
@@ -10,6 +11,9 @@ class App extends React.Component {
             <BrowserRouter>
                 <Route exact path="/">
                     <Homepage />
+                </Route>
+                <Route path="/about">
+                    <About />
                 </Route>
             </BrowserRouter>
         );
