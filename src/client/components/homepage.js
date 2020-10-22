b75f5bfb760aa717d846a22cebf45ac92afff62f
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-22 16:42:55 +0200

    Surround the return content in a div

--- src/client/components/homepage.js
+++ src/client/components/homepage.js
@@ -5,12 +5,12 @@ import Header from "./header";
 class Homepage extends React.Component {
     render() {
         return (
-            <>
+            <div className={"homepage__container"}>
                 <Header />
                 <div className={"title"}>
                     <h1 className={"title__main-title"}>{"ARNAUD"}<br />{"DE BAERDEMAEKER"}</h1>
                 </div>
-            </>
+            </div>
         );
     }
 }
