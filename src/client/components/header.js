86d1aa08b20c71ed655a2844cd42f8a359e03c85
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-22 09:47:40 +0200

    Put the nav links into the Nav component

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,18 +1,13 @@
 /* eslint-disable class-methods-use-this */
 import React from "react";
 import {hot} from "react-hot-loader/root";
+import Nav from "./nav";
 
 class Header extends React.Component {
     render() {
         return (
             <header className={"menu"}>
-                <nav className={"menu__navigation"}>
-                    <ul className={"menu__list"}>
-                        <li className={"menu__list-item"}>{"Accueil"}</li>
-                        <li className={"menu__list-item"}>{"Galerie"}</li>
-                        <li className={"menu__list-item"}>{"À propos"}</li>
-                    </ul>
-                </nav>
+                <Nav />
             </header>
         );
     }
