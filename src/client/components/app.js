f314f0347c85812c3a9a8aaf1b05bfb6ddaa82ea
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-22 09:46:24 +0200

    Add the router
    Called the components of react-router-dom.
    Added the Homepage component which contains Header and Main components.

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,16 +1,17 @@
 /* eslint-disable class-methods-use-this */
 import React from "react";
 import {hot} from "react-hot-loader/root";
-import Header from "./header";
-import Main from "./main-content";
+import Homepage from "./homepage.js";
+import {BrowserRouter, Route} from "react-router-dom";
 
 class App extends React.Component {
     render() {
         return (
-            <>
-                <Header />
-                <Main />
-            </>
+            <BrowserRouter>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+            </BrowserRouter>
         );
     }
 }
