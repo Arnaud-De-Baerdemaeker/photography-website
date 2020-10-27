dd8d2c09a27033604a4c5bb738dab725810ad84f
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-27 10:45:09 +0100

    Pass props to Header component

--- src/client/components/home-page/home.js
+++ src/client/components/home-page/home.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import Header from "./../header/header";
 
-class HomePage extends React.Component {
+class HomePage extends Component {
     render() {
         return (
             <div className={"home-page__container"}>
-                <Header />
+                <Header
+                    onHomePage={this.props.onHomePage}
+                    changeState={this.props.changeState}
+                />
                 <div className={"title"}>
                     <h1 className={"title__main-title"}>{"ARNAUD"}<br />{"DE BAERDEMAEKER"}</h1>
                 </div>
