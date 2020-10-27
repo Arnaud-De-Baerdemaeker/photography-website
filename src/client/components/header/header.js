3e961c8b7c3086496e51b0bc7442594f6eeeab78
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-27 10:44:03 +0100

    Moved the nav content into the Header component
    Added the class names that switches with the state.
    Call the function changeState on click on the NavLink.
    Set activeClassName to disable the Link via CSS when active.

--- src/client/components/header/header.js
+++ src/client/components/header/header.js
@@ -1,13 +1,40 @@
 /* eslint-disable class-methods-use-this */
-import React from "react";
+import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
-import Nav from "./nav";
+import {NavLink} from "react-router-dom";
 
-class Header extends React.Component {
+class Header extends Component {
     render() {
         return (
-            <header className={"menu"}>
-                <Nav />
+            <header className={this.props.onHomePage ? "menu--home-page" : "menu--other-pages"}>
+                <nav className={this.props.onHomePage ? "menu__navigation--home-page" : "menu__navigation--other-pages"}>
+                    <ul className={this.props.onHomePage ? "menu__list--home-page" : "menu__list--other-pages"}>
+                        <li className={this.props.onHomePage ? "menu__list-item--home-page" : "menu__list-item--other-pages"}>
+                            <NavLink
+                                to="/"
+                                onClick={this.props.changeState}
+                                className={this.props.onHomePage ? "menu__link--home-page" : "menu__link--other-pages"}
+                                activeClassName={"menu__link--disabled"}
+                            >{"ACCUEIL"}</NavLink>
+                        </li>
+                        <li className={this.props.onHomePage ? "menu__list-item--home-page" : "menu__list-item--other-pages"}>
+                            <NavLink
+                                to="/gallery"
+                                onClick={this.props.changeState}
+                                className={this.props.onHomePage ? "menu__link--home-page" : "menu__link--other-pages"}
+                                activeClassName={"menu__link--disabled"}
+                            >{"GALERIE"}</NavLink>
+                        </li>
+                        <li className={this.props.onHomePage ? "menu__list-item--home-page" : "menu__list-item--other-pages"}>
+                            <NavLink
+                                to="/about"
+                                onClick={this.props.changeState}
+                                className={this.props.onHomePage ? "menu__link--home-page" : "menu__link--other-pages"}
+                                activeClassName={"menu__link--disabled"}
+                            >{"À PROPOS"}</NavLink>
+                        </li>
+                    </ul>
+                </nav>
             </header>
         );
     }
