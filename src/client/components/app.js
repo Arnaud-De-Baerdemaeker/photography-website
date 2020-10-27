295f5faf14aaf8611c012908e2b3f2980da84ed0
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-27 10:46:40 +0100

    Declare the constructor in the root component
    - Pass the state and the function to HomePage and About components.

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,19 +1,37 @@
 /* eslint-disable class-methods-use-this */
-import React from "react";
+import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import {BrowserRouter, Route} from "react-router-dom";
 import HomePage from "./home-page/home";
 import About from "./about-page/about";
 
-class App extends React.Component {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            onHomePage: true
+        };
+        this.changeState = this.changeState.bind(this);
+    }
+
+    changeState() {
+        this.setState({onHomePage: !this.state.onHomePage});
+    }
+
     render() {
         return (
             <BrowserRouter>
                 <Route exact path="/">
-                    <HomePage />
+                    <HomePage
+                        onHomePage={this.state.onHomePage}
+                        changeState={this.changeState}
+                    />
                 </Route>
                 <Route path="/about">
-                    <About />
+                    <About
+                        onHomePage={this.state.onHomePage}
+                        changeState={this.changeState}
+                    />
                 </Route>
             </BrowserRouter>
         );
