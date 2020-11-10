7f5c6daad737a5db264753d9b17400426407a30f
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-10 14:33:33 +0100

    Add more content to the Footer component
    Imported the mail, Linked In and GitHub icons.
    Surrounded the images by a link.
    Added the URLs to the links.
    Modified the copyright to credits.

--- src/client/components/shared/footer.js
+++ src/client/components/shared/footer.js
@@ -1,43 +1,65 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 
+import Mail from "./../../images/email-icon.svg";
+import LinkedIn from "./../../images/linkedin-icon.png";
+import GitHub from "./../../images/github-icon.png";
+
 class Footer extends Component {
     render() {
-        return (
+        return(
             <footer className={"footer"}>
                 <div className={"footer__left-box"}>
                     <ul className={"footer__links"}>
                         <li className={"footer__email"}>
-                            <img
-                                src={""}
-                                alt={"E-mail"}
-                                title={"Contactez-moi"}
-                                loading={"lazy"}
-                                className={"footer__email-icon"}
-                            />
+                            <a
+                                href={"mailto:de.baerdemaeker.arnaud@gmail.com"}
+                                target={"_blank"}
+                                className={"footer__link"}
+                            >
+                                <img
+                                    src={Mail}
+                                    alt={"E-mail"}
+                                    title={"Contactez-moi"}
+                                    loading={"lazy"}
+                                    className={"footer__email-icon"}
+                                />
+                            </a>
                         </li>
                         <li className={"footer__linkedin"}>
-                            <img
-                                src={""}
-                                alt={"LinkedIn"}
-                                title={"Mon profil LinkedIn"}
-                                loading={"lazy"}
-                                className={"footer__linkedin-icon"}
-                            />
+                            <a
+                                href={"https://www.linkedin.com/in/arnaud-de-baerdemaeker/"}
+                                target={"_target"}
+                                className={"footer__link"}
+                            >
+                                <img
+                                    src={LinkedIn}
+                                    alt={"LinkedIn"}
+                                    title={"Mon profil LinkedIn"}
+                                    loading={"lazy"}
+                                    className={"footer__linkedin-icon"}
+                                />
+                            </a>
                         </li>
                         <li className={"footer__github"}>
-                            <img
-                                src={""}
-                                alt={"GitHub"}
-                                title={"Mon travail sur GitHub"}
-                                loading={"lazy"}
-                                className={"footer__github-icon"}
-                            />
+                            <a
+                                href={"https://github.com/Arnaud-De-Baerdemaeker"}
+                                target={"_blank"}
+                                className={"footer__link"}
+                            >
+                                <img
+                                    src={GitHub}
+                                    alt={"GitHub"}
+                                    title={"Mon travail sur GitHub"}
+                                    loading={"lazy"}
+                                    className={"footer__github-icon"}
+                                />
+                            </a>
                         </li>
                     </ul>
                 </div>
                 <div className={"footer__right-box"}>
-                    <p className={"footer__copyrights"}>{""}</p>
+                    <p className={"footer__credits"}>{"2020 Réalisé par Arnaud De Baerdemaeker"}</p>
                 </div>
             </footer>
         );
