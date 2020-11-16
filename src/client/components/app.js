4d4716c9662847c4b6255fbd59d9a6231cde4fc0
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-16 14:15:15 +0100

    Add the route for the Gallery component

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -3,14 +3,18 @@ import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import {BrowserRouter, Route} from "react-router-dom";
 import HomePage from "./home-page/home";
+import Gallery from "./gallery-page/gallery";
 import About from "./about-page/about";
 
 class App extends Component {
     render() {
-        return (
+        return(
             <BrowserRouter>
                 <Route exact path="/">
                     <HomePage />
+                </Route>
+                <Route path="/gallery">
+                    <Gallery />
                 </Route>
                 <Route path="/about">
                     <About />
