b4431c790f3e89db826d477e08895df308cef309
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-24 16:24:00 +0100

    Build the metadata file for the photos

---
+++ src/client/javascript/metadata.js
@@ -0,0 +1,89 @@
+const photos2014 =
+[
+    {
+        title: 2014
+    }
+];
+
+const photos2015 =
+[
+    {
+        title: 2015
+    }
+];
+
+const photos2016 =
+[
+    {
+        title: 2016
+    },
+    {
+        url: "https://drive.google.com/uc?id=1p0NZkKgZO5Y2z7NRhZ4Df33ORh3nRd0Q",
+        name: "Parc de la Boverie",
+        day: "Vendredi",
+        date: "13 mai 2016",
+        time: "10:59:59",
+        aperture: "f/2,8",
+        shutter_speed: "1/1600s",
+        focal_length: 4.3,
+        iso: 80,
+        camera: "Sony",
+        model: "DSC-HX300"
+    },
+    {
+        url: "https://drive.google.com/uc?id=1NJbkAWFK2-2o_Dd-LB06-cu3E45J4LSK",
+        name: "Herbes glacées",
+        day: "Jeudi",
+        date: "29 décembre 2016",
+        time: "15:31:22",
+        aperture: "f/4,0",
+        shutter_speed: "1/100s",
+        focal_length: 15.6,
+        iso: 80,
+        camera: "Sony",
+        model: "DSC-HX300"
+    }
+];
+
+const photos2017 =
+[
+    {
+        title: 2017
+    }
+];
+
+const photos2018 =
+[
+    {
+        title: 2018
+    },
+    {
+        url: "https://drive.google.com/uc?id=1G30dFQ08efzxxnWvOeaeB3t8iouJs-KY",
+        name: "Lune",
+        day: "Vendredi",
+        date: "28 septembre 2018",
+        time: "22:14:34",
+        aperture: "f/6,3",
+        shutter_speed: "1/80s",
+        focal_length: 215.0,
+        iso: 80,
+        camera: "Sony",
+        model: "DSC-HX300"
+    }
+];
+
+const photos2019 =
+[
+    {
+        title: 2019
+    }
+];
+
+const photos2020 =
+[
+    {
+        title: 2020
+    }
+];
+
+export {photos2014, photos2015, photos2016, photos2017, photos2018, photos2019, photos2020};
