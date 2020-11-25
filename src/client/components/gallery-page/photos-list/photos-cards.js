2e4f0fd5c303bc344cc5d63bfcc886d1758021f0
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-25 16:34:34 +0100

    Add a new <div> to surround the card's data

--- src/client/components/gallery-page/photos-list/photos-cards.js
+++ src/client/components/gallery-page/photos-list/photos-cards.js
@@ -7,28 +7,30 @@ class PhotosCards extends Component
     {
         return(
             <>
-                <div className={"photos-cards__image-container"}>
-                    <img
-                        src={this.props.url}
-                        className={"photos-cards__image"}
-                    />
-                </div>
-                <div className={"photos-cards__metadata"}>
-                    <ul className={"photos-cards__list"}>
-                        <li className={"photos-cards__list-item"}>
-                            <h2 className={"photos-cards__title-2"}>{this.props.name}</h2>
-                        </li>
-                        <li className={"photos-cards__list-item"}>{this.props.place}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.day}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.date}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.time}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.aperture}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.shutter_speed}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.focal_length}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.iso}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.camera}</li>
-                        <li className={"photos-cards__list-item"}>{this.props.model}</li>
-                    </ul>
+                <div className={"photos-cards__card"}>
+                    <div className={"photos-cards__image-container"}>
+                        <img
+                            src={this.props.url}
+                            className={"photos-cards__image"}
+                        />
+                    </div>
+                    <div className={"photos-cards__metadata"}>
+                        <ul className={"photos-cards__list"}>
+                            <li className={"photos-cards__list-item"}>
+                                <h2 className={"photos-cards__title-2"}>{this.props.name}</h2>
+                            </li>
+                            <li className={"photos-cards__list-item"}>{this.props.place}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.day}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.date}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.time}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.aperture}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.shutter_speed}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.focal_length}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.iso}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.camera}</li>
+                            <li className={"photos-cards__list-item"}>{this.props.model}</li>
+                        </ul>
+                    </div>
                 </div>
             </>
         );
