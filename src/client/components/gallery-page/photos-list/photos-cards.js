d7f4df095ea157edc83ba59d9d7f1b905a4d31d0
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-26 16:47:34 +0100

    Modify the <PhotosCards> component
    - Gathered some data to gain some place.
    - Putted the title out of the list.
    - Added a className to <figcaption>.

--- src/client/components/gallery-page/photos-list/photos-cards.js
+++ src/client/components/gallery-page/photos-list/photos-cards.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, lazy} from "react";
 import {hot} from "react-hot-loader/root";
 
 class PhotosCards extends Component
@@ -12,25 +12,21 @@ class PhotosCards extends Component
                         <img
                             src={this.props.thumbnail}
                             alt={"Image non trouvée"}
+                            loading={lazy}
                             className={"photos-cards__image"}
                         />
-                        <figcaption>{this.props.author}</figcaption>
+                        <figcaption className={"photos-card__caption"}>{this.props.author}</figcaption>
                     </figure>
                     <div className={"photos-cards__metadata"}>
+                        <h2 className={"photos-cards__title-2"}>{this.props.name}</h2>
                         <ul className={"photos-cards__list"}>
-                            <li className={"photos-cards__list-item"}>
-                                <h2 className={"photos-cards__title-2"}>{this.props.name}</h2>
-                            </li>
+                            <li className={"photos-cards__list-item"}>{`${this.props.day} ${this.props.date}, ${this.props.time}`}</li>
                             <li className={"photos-cards__list-item"}>{this.props.place}</li>
-                            <li className={"photos-cards__list-item"}>{this.props.day}</li>
-                            <li className={"photos-cards__list-item"}>{this.props.date}</li>
-                            <li className={"photos-cards__list-item"}>{this.props.time}</li>
                             <li className={"photos-cards__list-item"}>{this.props.aperture}</li>
                             <li className={"photos-cards__list-item"}>{this.props.shutter_speed}</li>
                             <li className={"photos-cards__list-item"}>{this.props.focal_length}</li>
                             <li className={"photos-cards__list-item"}>{this.props.iso}</li>
                             <li className={"photos-cards__list-item"}>{this.props.camera}</li>
-                            <li className={"photos-cards__list-item"}>{this.props.model}</li>
                         </ul>
                     </div>
                 </div>
