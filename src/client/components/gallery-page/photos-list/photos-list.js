65a5903635f56a98a7f1b0ff408f5fac4b6bd42e
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-12-04 15:46:28 +0100

    Modify the <PhotosList> component
    - Corrected the imported files' names for 2 components.
    - Removed the props from the constructor.
    - Removed the date from state to use props directly.
    - Added a function on mount to restore the view to the top.
    - Added the imported components.
    - Added the class names to the tags.

--- src/client/components/gallery-page/photos-list/photos-list.js
+++ src/client/components/gallery-page/photos-list/photos-list.js
@@ -2,54 +2,75 @@ import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import Header from "../../shared/header";
 import Footer from "../../shared/footer";
-// import Divider from "./../shared/divider";
+import MyName from "./../../shared/name";
+import Divider from "./../../shared/divider";
 import PhotosCards from "./photos-cards";
 
 class PhotosList extends Component
 {
-    constructor(props)
+    constructor()
     {
-        super(props);
+        super();
+        
         this.state =
         {
-            // Store the props into a state
-            data: this.props.photos,
             // create an array
             card: []
         }
     }
 
+    componentDidMount()
+    {
+        // The condition checks if the value of the scroll is different than 0.
+        if (window.scrollY !== 0)
+        {
+            // If it is, it restores the view to the top.
+            window.scrollTo(0, 0);
+        }
+    }
+
     render()
     {
-        // The loop goes on following the numbers of entries in the metadata.js file
-        for (let i = 1; i < this.state.data.length; i++)
+        // The loop goes on following the number of entries in the metadata.js file
+        for (let i = 1; i < this.props.photos.length; i++)
         {
             // For each loop, an iteration of the <PhotoCards> component is created with, each time, different props. Then, the iteration is pushed in the array.
             this.state.card.push(
                 <PhotosCards
-                    url={this.state.data[i].url}
-                    name={this.state.data[i].name}
-                    day={this.state.data[i].day}
-                    date={this.state.data[i].date}
-                    time={this.state.data[i].time}
-                    aperture={this.state.data[i].aperture}
-                    shutter_speed={this.state.data[i].shutter_speed}
-                    focal_length={this.state.data[i].focal_length}
-                    iso={this.state.data[i].iso}
-                    camera={this.state.data[i].camera}
-                    model={this.state.data[i].model}
+                    key={i}
+                    full_image={this.props.photos[i].full_image}
+                    thumbnail={this.props.photos[i].thumbnail}
+                    author={this.props.photos[i].author}
+                    name={this.props.photos[i].name}
+                    place={this.props.photos[i].place}
+                    day={this.props.photos[i].day}
+                    date={this.props.photos[i].date}
+                    time={this.props.photos[i].time}
+                    aperture={this.props.photos[i].aperture}
+                    shutter_speed={this.props.photos[i].shutter_speed}
+                    focal_length={this.props.photos[i].focal_length}
+                    iso={this.props.photos[i].iso}
+                    camera={this.props.photos[i].camera}
+                    model={this.props.photos[i].model}
                 />
-            )
+            );
         }
 
         return(
             <>
                 <Header />
-                <div>
-                    <div>
-                        <h1 className={"main-title"}>{this.props.photos[0].title}</h1>
+                <div className={"photos-list"}>
+                    <MyName />
+                    <Divider
+                        dividerContainer={this.props.dividerContainer[1]}
+                        dividerImage={this.props.dividerImage[1]}
+                    />
+                    <div className={"photos-list__title-container"}>
+                        <h2 className={"photos-list__title-1"}>{this.props.photos[0].title}</h2>
                     </div>
-                    {this.state.card}
+                    <div className={"photos-list__cards"}>
+                        {this.state.card}
+                    </div>
                 </div>
                 <Footer />
             </>
