5ddd293353571d2a626ea966fd307db8d9f4e3fb
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-12-11 11:11:34 +0100

    Set the <Link> component
    - Created new component to fix eslint depth rule and reduce repetition of code.

---
+++ src/client/components/gallery-page/link.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import {hot} from "react-hot-loader/root";
+import {NavLink} from "react-router-dom";
+
+class Link extends Component {
+    render() {
+        return (
+            <li className={"gallery__list-item"}>
+                <NavLink to={this.props.link} className={"gallery__link"}>
+                    <img
+                        src={this.props.src}
+                        alt={this.props.alt}
+                        className={"gallery__thumbnail"}
+                    />
+                    <span className={"gallery__year"}>{this.props.year}</span>
+                </NavLink>
+            </li>
+        );
+    }
+}
+
+export default hot(Link);
