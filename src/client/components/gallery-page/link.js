30d3f7f4156e407f5b122d24e6de3cd41d3f61aa
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-12-16 16:26:00 +0100

    Add the <Image> component with props

--- src/client/components/gallery-page/link.js
+++ src/client/components/gallery-page/link.js
@@ -5,16 +5,17 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import {NavLink} from "react-router-dom";
+import Image from "./../shared/image";
 
 class Link extends Component {
     render() {
         return (
             <li className={"gallery__list-item"}>
                 <NavLink to={this.props.link} className={"gallery__link"}>
-                    <img
-                        src={this.props.src}
-                        alt={this.props.alt}
-                        className={"gallery__thumbnail"}
+                    <Image
+                        url={this.props.url}
+                        alternate_text={this.props.alternate_text}
+                        class={"gallery__thumbnail"}
                     />
                     <span className={"gallery__year"}>{this.props.year}</span>
                 </NavLink>
