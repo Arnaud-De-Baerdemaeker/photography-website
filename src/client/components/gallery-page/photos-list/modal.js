5546f24d9ca801a74f9abf1a1c168677e5e775ea
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-12-20 12:54:16 +0100

    Place the elements in a global <div>

--- src/client/components/gallery-page/photos-list/modal.js
+++ src/client/components/gallery-page/photos-list/modal.js
@@ -9,16 +9,14 @@ import Image from "./../../shared/image";
 class Modal extends Component {
     render() {
         return (
-            <>
+            <div className={"modal"}>
                 <figure className={"modal__image-container"}>
                     <Image url={this.props.url} class={"modal__image"} />
                     <figcaption className={"modal__caption"}>
                         {"Arnaud De Baerdemaeker"}
                     </figcaption>
                 </figure>
-                <div
-                    className={"modal__close"}
-                    onClick={this.props.onClick}>
+                <div className={"modal__close"} onClick={this.props.onClick}>
                     <img
                         src={
                             "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0id2hpdGUiIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjE4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMFYweiIgZmlsbD0ibm9uZSIvPjxwYXRoIGQ9Ik0xOC4zIDUuNzFjLS4zOS0uMzktMS4wMi0uMzktMS40MSAwTDEyIDEwLjU5IDcuMTEgNS43Yy0uMzktLjM5LTEuMDItLjM5LTEuNDEgMC0uMzkuMzktLjM5IDEuMDIgMCAxLjQxTDEwLjU5IDEyIDUuNyAxNi44OWMtLjM5LjM5LS4zOSAxLjAyIDAgMS40MS4zOS4zOSAxLjAyLjM5IDEuNDEgMEwxMiAxMy40MWw0Ljg5IDQuODljLjM5LjM5IDEuMDIuMzkgMS40MSAwIC4zOS0uMzkuMzktMS4wMiAwLTEuNDFMMTMuNDEgMTJsNC44OS00Ljg5Yy4zOC0uMzguMzgtMS4wMiAwLTEuNHoiLz48L3N2Zz4="
@@ -27,7 +25,7 @@ class Modal extends Component {
                         className={"modal__close-icon"}
                     />
                 </div>
-            </>
+            </div>
         );
     }
 }
