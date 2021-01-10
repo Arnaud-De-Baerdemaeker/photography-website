2c74ddcb4e14613f7af5a05248e00a036667e102
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-01-10 13:06:19 +0100

    Remove the componentDidMount
    - Placed the functions in the render.
    - Adapted the name of the props.
    - Added more data from the props.

--- src/client/components/gallery-page/photos-list/photos-list.js
+++ src/client/components/gallery-page/photos-list/photos-list.js
@@ -19,35 +19,36 @@ class PhotosList extends Component {
         };
     }
 
-    componentDidMount() {
+    render() {
         // The condition checks if the value of the scroll is different than 0.
         if (window.scrollY !== 0) {
             // If it is, it restores the view to the top.
             window.scrollTo(0, 0);
         }
-    }
 
-    render() {
-        // The loop goes on following the number of entries in the metadata.js file
-        for (let i = 1; i < this.props.photos.length; i++) {
+        // The loop goes on following the number of entries in the metadata.json file
+        for (let i = 0; i < this.props.album.photos.length; i++) {
             // For each loop, an iteration of the <PhotoCards> component is created with, each time, different props. Then, the iteration is pushed in the array.
             this.state.card.push(
                 <PhotosCards
                     key={i}
-                    full_image={this.props.photos[i].full_image}
-                    thumbnail={this.props.photos[i].thumbnail}
-                    author={this.props.photos[i].author}
-                    name={this.props.photos[i].name}
-                    place={this.props.photos[i].place}
-                    day={this.props.photos[i].day}
-                    date={this.props.photos[i].date}
-                    time={this.props.photos[i].time}
-                    aperture={this.props.photos[i].aperture}
-                    shutter_speed={this.props.photos[i].shutter_speed}
-                    focal_length={this.props.photos[i].focal_length}
-                    iso={this.props.photos[i].iso}
-                    camera={this.props.photos[i].camera}
-                    model={this.props.photos[i].model}
+                    full_image={this.props.album.photos[i].full_image}
+                    thumbnail={this.props.album.photos[i].thumbnail}
+                    author={this.props.album.photos[i].author}
+                    name={this.props.album.photos[i].name}
+                    place={this.props.album.photos[i].place}
+                    day={this.props.album.photos[i].day}
+                    date={this.props.album.photos[i].date}
+                    month={this.props.album.photos[i].month}
+                    year={this.props.album.photos[i].year}
+                    hours={this.props.album.photos[i].hours}
+                    minutes={this.props.album.photos[i].minutes}
+                    aperture={this.props.album.photos[i].aperture}
+                    shutter_speed={this.props.album.photos[i].shutter_speed}
+                    focal_length={this.props.album.photos[i].focal_length}
+                    iso={this.props.album.photos[i].iso}
+                    camera={this.props.album.photos[i].camera}
+                    model={this.props.album.photos[i].model}
                 />,
             );
         }
@@ -63,7 +64,7 @@ class PhotosList extends Component {
                     />
                     <div className={"photos-list__title-container"}>
                         <h2 className={"photos-list__title-1"}>
-                            {this.props.photos[0].title}
+                            {this.props.album.title}
                         </h2>
                     </div>
                     <Divider
