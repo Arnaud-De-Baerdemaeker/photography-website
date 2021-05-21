075ebf579fb02c1654dedaf983d25ef5bfeee041
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-05-21 13:46:26 +0200

    Refactor: Remove unused lines
    - Removed the constructor which is no longer needed
    - Removed the divider's props passed in the router

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -12,73 +12,33 @@ import PhotosList from "./photos-list/photos-list";
 import data from "./../JSON/metadata.json";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            dividerContainer: ["divider--top", "divider--bottom"],
-            dividerImage: ["divider__image--top", "divider__image--bottom"],
-        };
-    }
-
     render() {
         return (
             <BrowserRouter>
                 <Switch>
                     <Route exact path={"/gallery/2014"}>
-                        <PhotosList
-                            album={data.album_2014}
-                            dividerContainer={this.state.dividerContainer}
-                            dividerImage={this.state.dividerImage}
-                        />
+                        <PhotosList album={data.album_2014} />
                     </Route>
                     <Route exact path={"/gallery/2015"}>
-                        <PhotosList
-                            album={data.album_2015}
-                            dividerContainer={this.state.dividerContainer}
-                            dividerImage={this.state.dividerImage}
-                        />
+                        <PhotosList album={data.album_2015} />
                     </Route>
                     <Route exact path={"/gallery/2016"}>
-                        <PhotosList
-                            album={data.album_2016}
-                            dividerContainer={this.state.dividerContainer}
-                            dividerImage={this.state.dividerImage}
-                            location
-                        />
+                        <PhotosList album={data.album_2016} location />
                     </Route>
                     <Route exact path={"/gallery/2017"}>
-                        <PhotosList
-                            album={data.album_2017}
-                            dividerContainer={this.state.dividerContainer}
-                            dividerImage={this.state.dividerImage}
-                        />
+                        <PhotosList album={data.album_2017} />
                     </Route>
                     <Route exact path={"/gallery/2018"}>
-                        <PhotosList
-                            album={data.album_2018}
-                            dividerContainer={this.state.dividerContainer}
-                            dividerImage={this.state.dividerImage}
-                        />
+                        <PhotosList album={data.album_2018} />
                     </Route>
                     <Route exact path={"/gallery/2019"}>
-                        <PhotosList
-                            album={data.album_2019}
-                            dividerContainer={this.state.dividerContainer}
-                            dividerImage={this.state.dividerImage}
-                        />
+                        <PhotosList album={data.album_2019} />
                     </Route>
                     <Route exact path={"/gallery"}>
-                        <Gallery
-                            dividerContainer={this.state.dividerContainer}
-                            dividerImage={this.state.dividerImage}
-                        />
+                        <Gallery />
                     </Route>
                     <Route exact path={"/about"}>
-                        <About
-                            dividerContainer={this.state.dividerContainer}
-                            dividerImage={this.state.dividerImage}
-                        />
+                        <About />
                     </Route>
                     <Route exact path={"/"}>
                         <HomePage />
