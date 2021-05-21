273d3420f4e6360ab5be1ef718f82045b042a35e
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-05-21 12:13:26 +0200

    Refactor: Modify the navigation component
    - Changed the name of the component
    - Changed the position of the JS and SCSS files in the project

---
+++ src/client/components/navigation/navigation.js
@@ -0,0 +1,96 @@
+// Photography website
+// Started on July 2020
+// By Arnaud De Baerdemaeker
+
+import React, {Component} from "react";
+import {hot} from "react-hot-loader/root";
+import {NavLink} from "react-router-dom";
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            // Recover and store the current url of the page
+            url: window.location.toString(),
+            // Store a target url
+            path: "http://localhost:8080/",
+        };
+    }
+
+    render() {
+        return (
+            // The 2 previous constants are compared against each other to know which CSS class to render following the page rendered
+            <nav
+                className={
+                    this.state.url === this.state.path
+                        ? "menu--home-page"
+                        : "menu--other-pages"
+                }>
+                <ul
+                    className={
+                        this.state.url === this.state.path
+                            ? "menu__list--home-page"
+                            : "menu__list--other-pages"
+                    }>
+                    <li
+                        className={
+                            this.state.url === this.state.path
+                                ? "menu__list-item--home-page"
+                                : "menu__list-item--other-pages"
+                        }>
+                        <NavLink
+                            to={"/"}
+                            // Allows to exactly match the url set in the "to" attribute
+                            exact={true}
+                            // Class name that is active only when the page is active
+                            activeClassName={"menu__link--active"}
+                            className={
+                                this.state.url === this.state.path
+                                    ? "menu__link--home-page"
+                                    : "menu__link--other-pages"
+                            }>
+                            {"ACCUEIL"}
+                        </NavLink>
+                    </li>
+                    <li
+                        className={
+                            this.state.url === this.state.path
+                                ? "menu__list-item--home-page"
+                                : "menu__list-item--other-pages"
+                        }>
+                        <NavLink
+                            to={"/gallery"}
+                            activeClassName={"menu__link--active"}
+                            className={
+                                this.state.url === this.state.path
+                                    ? "menu__link--home-page"
+                                    : "menu__link--other-pages"
+                            }>
+                            {"GALERIE"}
+                        </NavLink>
+                    </li>
+                    <li
+                        className={
+                            this.state.url === this.state.path
+                                ? "menu__list-item--home-page"
+                                : "menu__list-item--other-pages"
+                        }>
+                        <NavLink
+                            to={"/about"}
+                            activeClassName={"menu__link--active"}
+                            className={
+                                this.state.url === this.state.path
+                                    ? "menu__link--home-page"
+                                    : "menu__link--other-pages"
+                            }>
+                            {"À PROPOS"}
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default hot(Navigation);
