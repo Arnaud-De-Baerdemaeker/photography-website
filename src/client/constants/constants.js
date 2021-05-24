dd47b82f4e53fdecbe95c3020b1f3395a2f60ba2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-05-24 16:45:14 +0200

    Refactor: Store constants into a separate file

---
+++ src/client/constants/constants.js
@@ -0,0 +1,12 @@
+const galleryInputs = {
+    headerTitle: "Galerie",
+    introduction: {
+        title: "Bienvenue dans la galerie !",
+        phrases: [
+            "Dans cette section, vous allez pouvoir découvrir une sélection de mes photos.",
+            "Choisissez une année pour afficher les photos correspondantes.",
+        ],
+    },
+};
+
+export {galleryInputs};
