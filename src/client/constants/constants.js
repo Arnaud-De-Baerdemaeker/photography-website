b19f5aa47d8099b0c9d7d10e2fa9210431cc0072
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-05-25 16:32:12 +0200

    Refactor: Change a word

--- src/client/constants/constants.js
+++ src/client/constants/constants.js
@@ -3,7 +3,7 @@ const galleryInputs = {
     introduction: {
         title: "Bienvenue dans la galerie !",
         phrases: [
-            "Dans cette section, vous allez pouvoir découvrir une sélection de mes photos.",
+            "Dans cette section, vous allez pouvoir découvrir une sélection de mes captures.",
             "Choisissez une année pour afficher les photos correspondantes.",
         ],
     },
