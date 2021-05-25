6795e531b10e78b67d6c102a625e36cabeef2c08
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-05-25 16:23:43 +0200

    Refactor: Simplify code
    - Removed constructor that is no longer needed
    - Removed ternary condition that is no longer needed
    - Renamed some class names

--- src/client/components/navigation/navigation.js
+++ src/client/components/navigation/navigation.js
@@ -7,83 +7,36 @@ import {hot} from "react-hot-loader/root";
 import {NavLink} from "react-router-dom";
 
 class Navigation extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            // Recover and store the current url of the page
-            url: window.location.toString(),
-            // Store a target url
-            path: "http://localhost:8080/",
-        };
-    }
-
     render() {
         return (
-            // The 2 previous constants are compared against each other to know which CSS class to render following the page rendered
-            <nav
-                className={
-                    this.state.url === this.state.path
-                        ? "menu--home-page"
-                        : "menu--other-pages"
-                }>
-                <ul
-                    className={
-                        this.state.url === this.state.path
-                            ? "menu__list--home-page"
-                            : "menu__list--other-pages"
-                    }>
-                    <li
-                        className={
-                            this.state.url === this.state.path
-                                ? "menu__list-item--home-page"
-                                : "menu__list-item--other-pages"
-                        }>
+            <nav className={"menu__container"}>
+                <ul className={"menu__list"}>
+                    <li className={"menu__list-item"}>
                         <NavLink
                             to={"/"}
                             // Allows to exactly match the url set in the "to" attribute
                             exact={true}
                             // Class name that is active only when the page is active
                             activeClassName={"menu__link--active"}
-                            className={
-                                this.state.url === this.state.path
-                                    ? "menu__link--home-page"
-                                    : "menu__link--other-pages"
-                            }>
+                            className={"menu__link"}>
                             {"ACCUEIL"}
                         </NavLink>
                     </li>
-                    <li
-                        className={
-                            this.state.url === this.state.path
-                                ? "menu__list-item--home-page"
-                                : "menu__list-item--other-pages"
-                        }>
+                    <li className={"menu__list-item"}>
                         <NavLink
                             to={"/gallery"}
+                            // Class name that is active only when the page is active
                             activeClassName={"menu__link--active"}
-                            className={
-                                this.state.url === this.state.path
-                                    ? "menu__link--home-page"
-                                    : "menu__link--other-pages"
-                            }>
+                            className={"menu__link"}>
                             {"GALERIE"}
                         </NavLink>
                     </li>
-                    <li
-                        className={
-                            this.state.url === this.state.path
-                                ? "menu__list-item--home-page"
-                                : "menu__list-item--other-pages"
-                        }>
+                    <li className={"menu__list-item"}>
                         <NavLink
                             to={"/about"}
+                            // Class name that is active only when the page is active
                             activeClassName={"menu__link--active"}
-                            className={
-                                this.state.url === this.state.path
-                                    ? "menu__link--home-page"
-                                    : "menu__link--other-pages"
-                            }>
+                            className={"menu__link"}>
                             {"À PROPOS"}
                         </NavLink>
                     </li>
