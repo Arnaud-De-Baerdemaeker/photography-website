99b10125b901ad8f1e4d2c616fef4419f80648d0
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-05-28 13:02:04 +0200

    [Refactor]: Rearrange code
    - Use Google Icons for e-mail icon
    - Use JavaScript to dynamically get the full year

--- src/client/components/footer/footer.js
+++ src/client/components/footer/footer.js
@@ -5,11 +5,12 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 import {faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
 
 class Footer extends Component {
     render() {
+        const year = new Date();
+
         return (
             <footer className={"footer container"}>
                 <div className={"footer__left-box"}>
@@ -18,12 +19,12 @@ class Footer extends Component {
                             <a
                                 href={"mailto:de.baerdemaeker.arnaud@gmail.com"}
                                 className={"footer__link"}>
-                                <FontAwesomeIcon
-                                    icon={faEnvelope}
-                                    alt={"E-mail"}
-                                    title={"Contactez-moi"}
-                                    className={"footer__icon"}
-                                />
+                                <i
+                                    className={"material-icons"}
+                                    alt={"Email"}
+                                    title={"Contactez-moi"}>
+                                    {"email"}
+                                </i>
                             </a>
                         </li>
                         <li className={"footer__network"}>
@@ -58,7 +59,7 @@ class Footer extends Component {
                 </div>
                 <div className={"footer__right-box"}>
                     <p className={"footer__credits"}>
-                        {"2020 Arnaud De Baerdemaeker"}
+                        {`${year.getUTCFullYear()} Arnaud De Baerdemaeker`}
                         <br />
                         {"Tous droits réservés"}
                     </p>
