341358b9ba2c678ff6e437a3e33a434afe9d86bf
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-06-03 17:02:29 +0200

    [Refactor]: Add blur effect

--- src/client/components/image/image.js
+++ src/client/components/image/image.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import {LazyLoadImage} from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 class Image extends Component {
     render() {
@@ -10,6 +11,7 @@ class Image extends Component {
                 alt={this.props.alternate_text}
                 title={this.props.title}
                 threshold={100}
+                effect={"blur"}
                 onClick={this.props.onClick}
                 className={this.props.class}
             />
