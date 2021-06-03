e3050254207a96262d38409f17916ef3e7fd51f1
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-06-03 17:06:33 +0200

    [Refactor]: Rearrange code
    - Moved the title and location above the image
    - Moved the onClick function on the button component
    - Modified some class names

--- src/client/components/photos-cards/photos-cards.js
+++ src/client/components/photos-cards/photos-cards.js
@@ -5,6 +5,7 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import Image from "../image/image";
+import Button from "../button/button.js";
 import Modal from "../modal/modal";
 
 class PhotosCards extends Component {
@@ -25,27 +26,27 @@ class PhotosCards extends Component {
         return (
             <>
                 <div className={"photos-cards__card"}>
+                    <div className={"photos-cards__titles container"}>
+                        <h3 className={"photos-cards__mainTitle"}>
+                            {this.props.name}
+                        </h3>
+                        <h4 className={"photos-cards__subtitle"}>
+                            {this.props.place}
+                        </h4>
+                    </div>
                     <figure className={"photos-cards__image-container"}>
                         <Image
                             url={this.props.thumbnail}
-                            // On click, it displays the full image in a modal
-                            onClick={this.handleOnClick}
                             class={"photos-cards__image"}
                         />
-                        <figcaption className={"photos-card__caption"}>
+                        <figcaption className={"photos-cards__caption"}>
                             {this.props.author}
                         </figcaption>
                     </figure>
-                    <div className={"photos-cards__metadata"}>
-                        <h3 className={"photos-cards__title-2"}>
-                            {this.props.name}
-                        </h3>
+                    <div className={"photos-cards__metadata container"}>
                         <ul className={"photos-cards__list"}>
                             <li className={"photos-cards__list-item"}>
                                 {`${this.props.day} ${this.props.date} ${this.props.month} ${this.props.year}, ${this.props.hours}:${this.props.minutes}`}
-                            </li>
-                            <li className={"photos-cards__list-item"}>
-                                {this.props.place}
                             </li>
                             <li className={"photos-cards__list-item"}>
                                 {`Ouverture : f/${this.props.aperture}`}
@@ -64,6 +65,11 @@ class PhotosCards extends Component {
                             </li>
                         </ul>
                     </div>
+                    <Button
+                        // On click, it displays the full image in a modal
+                        onClick={this.handleOnClick}
+                        text={"Voir"}
+                    />
                 </div>
                 {/* Modal that is active only when the user clicks on the thumbnail */}
                 {this.state.modalIsOpen ? (
