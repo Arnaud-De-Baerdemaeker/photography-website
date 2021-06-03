c78d4774c84e7f5cf9192baa2b306894a8936380
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-06-03 16:59:39 +0200

    [Refactor]: Rearrange code
    - Added condition to check if there's a subtitle in the props
    - Putted the titles in a <div>

--- src/client/components/header/header.js
+++ src/client/components/header/header.js
@@ -7,12 +7,25 @@ import {hot} from "react-hot-loader/root";
 
 class Header extends Component {
     render() {
+        const subtitle = [];
+
+        if (this.props.headerSubtitle) {
+            subtitle.push(
+                <h3 className={"header__subtitle"}>
+                    {this.props.headerSubtitle}
+                </h3>,
+            );
+        }
+
         return (
             <header className={"header__container"}>
                 <h1 className={"header__main-title--other-pages"}>
                     {"Arnaud De Baerdemaeker"}
                 </h1>
-                <h2>{this.props.headerTitle}</h2>
+                <div className={"header__title"}>
+                    <h2>{this.props.headerTitle}</h2>
+                    {subtitle}
+                </div>
             </header>
         );
     }
