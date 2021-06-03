f614892a468d85bbeafba438b49aa6ccb814d848
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-06-03 10:49:40 +0200

    [Refactor]: Move the fixed text into a separate file

--- src/client/constants/constants.js
+++ src/client/constants/constants.js
@@ -1,12 +1,52 @@
-const galleryInputs = {
+const galleryPageInputs = {
     headerTitle: "Galerie",
-    introduction: {
-        title: "Bienvenue dans la galerie !",
-        phrases: [
-            "Dans cette section, vous allez pouvoir découvrir une sélection de mes captures.",
-            "Choisissez une année pour afficher les photos correspondantes.",
-        ],
-    },
+    section: [
+        {
+            title: "Bienvenue dans la galerie !",
+            phrases: [
+                "Dans cette section, vous allez pouvoir découvrir une sélection de mes captures.",
+                "Choisissez une année pour afficher les photos correspondantes.",
+            ],
+        },
+    ],
 };
 
-export {galleryInputs};
+const aboutMePageInputs = {
+    headerTitle: "À Propos",
+    sections: [
+        {
+            title: "Qui suis-je ?",
+            phrases: [
+                "Bonjour ! Je m'appelle Arnaud.",
+                "Vivant à Liège, en Belgique, je suis développeur web et accessoirement je pratique la photographie comme loisir.",
+                "Au fil de mon apprentissage des technologies de développement web, s'est imposé l'idée de fusionner ces deux domaines dans le but, d'un côté, de mettre en application ce que j'ai appris, et de l'autre, de pouvoir exposer les fruits de ce loisir.",
+            ],
+        },
+        {
+            title: "Pourquoi le développement web ?",
+            phrases: [
+                "J'ai découvert ce domaine un peu par hasard et j'ai été rapidement conquis.",
+                "Aujourd'hui, bien que touche-à-tout, j'affectionne particulièrement le fait de construire des designs, rédiger des lignes de code, admirer leur logique, et les voir prendre vie dans le navigateur.",
+                "Réfléchir aux couleurs à utiliser, à la disposition des différents éléments, à l'expérience utilisateur, sont autant de problématiques que j'apprécie de résoudre soigneusement.",
+            ],
+        },
+        {
+            title: "Pourquoi la photographie ?",
+            phrases: [
+                "Immortaliser l'instant. Montrer la beauté dans des choses ordinaires. Voilà les choses qui me plaisent dans la photographie.",
+                "La nature sous toutes ses formes et les architectures sont les types de sujets qui m'inspirent le plus.",
+            ],
+        },
+        {
+            title: "Et en dehors de ça ?",
+            phrases: [
+                "Quand je ne code et ne photographie pas, je prend plaisir à faire du vélo, jouer aux jeux vidéo, lire ou tout simplement me balader entre-autres.",
+            ],
+        },
+        {
+            title: "Mettez-vous à l'aise et découvrez...",
+        },
+    ],
+};
+
+export {galleryPageInputs, aboutMePageInputs};
