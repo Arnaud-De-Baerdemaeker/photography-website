1f3d5d9bf3c368ffd27afb1d8114e6c6434f95b2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-06-07 18:16:27 +0200

    [Refactor]: Create Button component to handle the modal
    - Moved the state to the Button component

--- src/client/components/button/button.js
+++ src/client/components/button/button.js
@@ -1,12 +1,42 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
+import Modal from "../modal/modal";
 
 class Button extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalIsOpen: false,
+        };
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    // Function that changes the boolean state each time it is called
+    handleOnClick() {
+        this.setState(state => ({
+            modalIsOpen: !state.modalIsOpen,
+        }));
+    }
+
     render() {
         return (
-            <div className={"button__container"}>
-                <button className={"button__item"}>{this.props.text}</button>
-            </div>
+            <>
+                <div className={"button__container"}>
+                    <button
+                        type={"button"}
+                        onClick={this.handleOnClick}
+                        className={"button__item"}>
+                        {"Voir"}
+                    </button>
+                </div>
+                {/* Modal that is active only when the user clicks on the thumbnail */}
+                {this.state.modalIsOpen ? (
+                    <Modal
+                        url={this.props.fullImages}
+                        onClick={this.handleOnClick}
+                    />
+                ) : null}
+            </>
         );
     }
 }
