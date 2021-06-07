2415e60cc5f2632723476d2654adec99b5ab9bb8
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-06-07 18:18:56 +0200

    [Refactor]: Add new elements
    - Added key-value pairs on the repeatable HTML tags
    - Added a divider
    - Replaced the urls, alts and years of the Links with props

--- src/client/components/gallery-page/gallery.js
+++ src/client/components/gallery-page/gallery.js
@@ -23,14 +23,19 @@ class Gallery extends Component {
 
         for (let i = 0; i < galleryPageInputs.sections.length; i++) {
             introductionSection.push(
-                <h3 className={"introduction__title"}>
+                <h3
+                    key={galleryPageInputs.sections[i].title}
+                    className={"introduction__title"}>
                     {galleryPageInputs.sections[i].title}
                 </h3>,
+                <div className={"divider"} />,
             );
             if ("phrases" in galleryPageInputs.sections[i]) {
                 for (const paragraph of galleryPageInputs.sections[i].phrases) {
                     introductionSection.push(
-                        <p className={"introduction__paragraph"}>
+                        <p
+                            key={paragraph}
+                            className={"introduction__paragraph"}>
                             {paragraph}
                         </p>,
                     );
@@ -50,51 +55,39 @@ class Gallery extends Component {
                         <ul className={"gallery__list"}>
                             <Link
                                 link={"/gallery/2014"}
-                                url={
-                                    "https://drive.google.com/uc?id=1FngJTHyYW4L07yL4UGMernAaaqQQsS42"
-                                }
-                                alternate_text={"2014"}
-                                year={"2014"}
+                                url={this.props.thumbnail2014}
+                                alternate_text={this.props.title2014}
+                                year={this.props.title2014}
                             />
                             <Link
                                 link={"/gallery/2015"}
-                                url={
-                                    "https://drive.google.com/uc?id=1cNj8fqVrLnoDzyUC5fPJ2OGE0WC4nYkA"
-                                }
-                                alternate_text={"2015"}
-                                year={"2015"}
+                                url={this.props.thumbnail2015}
+                                alternate_text={this.props.title2015}
+                                year={this.props.title2015}
                             />
                             <Link
                                 link={"/gallery/2016"}
-                                url={
-                                    "https://drive.google.com/uc?id=1E7yZKEU2g1ZtFf2vPLa2Z-H2NG_DWWIJ"
-                                }
-                                alternate_text={"2016"}
-                                year={"2016"}
+                                url={this.props.thumbnail2016}
+                                alternate_text={this.props.title2016}
+                                year={this.props.title2016}
                             />
                             <Link
                                 link={"/gallery/2017"}
-                                url={
-                                    "https://drive.google.com/uc?id=1UplsD-lSVjuKkCJ126wjINBWnEPrDb-W"
-                                }
-                                alternate_text={"2017"}
-                                year={"2017"}
+                                url={this.props.thumbnail2017}
+                                alternate_text={this.props.title2017}
+                                year={this.props.title2017}
                             />
                             <Link
                                 link={"/gallery/2018"}
-                                url={
-                                    "https://drive.google.com/uc?id=14_2JeZ5CNmIUpqA8XRGPM3oph9uNr_dn"
-                                }
-                                alternate_text={"2018"}
-                                year={"2018"}
+                                url={this.props.thumbnail2018}
+                                alternate_text={this.props.title2018}
+                                year={this.props.title2018}
                             />
                             <Link
                                 link={"/gallery/2019"}
-                                url={
-                                    "https://drive.google.com/uc?id=1X_cL_IzABX9I_DuEOv33yWR4e04JnYr5"
-                                }
-                                alternate_text={"2019"}
-                                year={"2019"}
+                                url={this.props.thumbnail2019}
+                                alternate_text={this.props.title2019}
+                                year={this.props.title2019}
                             />
                         </ul>
                     </div>
