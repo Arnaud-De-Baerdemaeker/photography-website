cf3ae3afe2e1771df20ba48885c93aa4c1360742
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-06-07 18:30:32 +0200

    [Add]: Pass the titles and thumbnails images to the Gallery component

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -44,7 +44,32 @@ class App extends Component {
                             <PhotosList album={data[5].album_2019} />
                         </Route>
                         <Route exact path={"/gallery"}>
-                            <Gallery />
+                            <Gallery
+                                title2014={data[0].album_2014.title}
+                                thumbnail2014={
+                                    data[0].album_2014.photos[0]["576p"]
+                                }
+                                title2015={data[1].album_2015.title}
+                                thumbnail2015={
+                                    data[1].album_2015.photos[0]["576p"]
+                                }
+                                title2016={data[2].album_2016.title}
+                                thumbnail2016={
+                                    data[2].album_2016.photos[0]["576p"]
+                                }
+                                title2017={data[3].album_2017.title}
+                                thumbnail2017={
+                                    data[3].album_2017.photos[0]["576p"]
+                                }
+                                title2018={data[4].album_2018.title}
+                                thumbnail2018={
+                                    data[4].album_2018.photos[0]["576p"]
+                                }
+                                title2019={data[5].album_2019.title}
+                                thumbnail2019={
+                                    data[5].album_2019.photos[0]["576p"]
+                                }
+                            />
                         </Route>
                         <Route exact path={"/about"}>
                             <About />
