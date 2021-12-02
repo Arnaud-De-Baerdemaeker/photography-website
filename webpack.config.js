4ff696efd966a2e81d7e641f53589a12aad5b5e9
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-02 14:35:59 +0100

    [chore] Set a different port to avoid conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,58 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack"); // eslint-disable-line no-unused-vars
+const path = require("path");
 
 const config = {
-  entry: [
-    'react-hot-loader/patch',
-    './src/client/index.js'
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(jpg|svg)$/,
-        use: 'file-loader'
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              mimetype: 'image/png'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        use: 'file-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ],
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
-  },
-  devServer: {
-    contentBase: './dist'
-  }
+    entry: ["react-hot-loader/patch", "./src/client/index.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(jpg|svg)$/,
+                use: "file-loader",
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            mimetype: "image/png",
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                use: "file-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+        alias: {
+            "react-dom": "@hot-loader/react-dom",
+        },
+    },
+    devServer: {
+        contentBase: "./dist",
+        port: 4000,
+    },
 };
 
 module.exports = config;
