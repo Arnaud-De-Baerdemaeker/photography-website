2421c4aefd193271c63c8914be6c31cd8047b69d
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-20 09:57:02 +0100

    [refactor] Rework the component
    - Remove the light/dark mode control and all its props
    - Remove the url for Google icons

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -12,20 +12,6 @@ import PhotosList from "./photos-list/photos-list";
 import data from "./../JSON/metadata.json";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            lightModeOn: true,
-        };
-        this.handleMode = this.handleMode.bind(this);
-    }
-
-    handleMode() {
-        this.setState(state => ({
-            lightModeOn: !state.lightModeOn,
-        }));
-    }
-
     render() {
         return (
             <>
@@ -33,12 +19,6 @@ class App extends Component {
                     <meta
                         name={"viewport"}
                         content={"width=device-width, initial-scale=1.0"}
-                    />
-                    <link
-                        href={
-                            "https://fonts.googleapis.com/icon?family=Material+Icons"
-                        }
-                        rel={"stylesheet"}
                     />
                     <link
                         rel={"preconnect"}
@@ -51,7 +31,7 @@ class App extends Component {
                     />
                     <link
                         href={
-                            "https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Bebas+Neue&family=Comfortaa:wght@300;400;500;600;700&display=swap"
+                            "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Comfortaa:wght@300;400;500;600;700&family=DM+Sans:wght@400;500;700&display=swap"
                         }
                         rel={"stylesheet"}
                     />
@@ -60,46 +40,22 @@ class App extends Component {
                 <BrowserRouter>
                     <Switch>
                         <Route exact path={"/gallery/2014"}>
-                            <PhotosList
-                                album={data[0].album_2014}
-                                lightModeOn={this.state.lightModeOn}
-                                onHandleMode={this.handleMode}
-                            />
+                            <PhotosList album={data[0].album_2014} />
                         </Route>
                         <Route exact path={"/gallery/2015"}>
-                            <PhotosList
-                                album={data[1].album_2015}
-                                lightModeOn={this.state.lightModeOn}
-                                onHandleMode={this.handleMode}
-                            />
+                            <PhotosList album={data[1].album_2015} />
                         </Route>
                         <Route exact path={"/gallery/2016"}>
-                            <PhotosList
-                                album={data[2].album_2016}
-                                lightModeOn={this.state.lightModeOn}
-                                onHandleMode={this.handleMode}
-                            />
+                            <PhotosList album={data[2].album_2016} />
                         </Route>
                         <Route exact path={"/gallery/2017"}>
-                            <PhotosList
-                                album={data[3].album_2017}
-                                lightModeOn={this.state.lightModeOn}
-                                onHandleMode={this.handleMode}
-                            />
+                            <PhotosList album={data[3].album_2017} />
                         </Route>
                         <Route exact path={"/gallery/2018"}>
-                            <PhotosList
-                                album={data[4].album_2018}
-                                lightModeOn={this.state.lightModeOn}
-                                onHandleMode={this.handleMode}
-                            />
+                            <PhotosList album={data[4].album_2018} />
                         </Route>
                         <Route exact path={"/gallery/2019"}>
-                            <PhotosList
-                                album={data[5].album_2019}
-                                lightModeOn={this.state.lightModeOn}
-                                onHandleMode={this.handleMode}
-                            />
+                            <PhotosList album={data[5].album_2019} />
                         </Route>
                         <Route exact path={"/gallery"}>
                             <Gallery
@@ -127,15 +83,10 @@ class App extends Component {
                                 thumbnail2019={
                                     data[5].album_2019.photos[0]["576p"]
                                 }
-                                lightModeOn={this.state.lightModeOn}
-                                onHandleMode={this.handleMode}
                             />
                         </Route>
                         <Route exact path={"/"}>
-                            <HomePage
-                                lightModeOn={this.state.lightModeOn}
-                                onHandleMode={this.handleMode}
-                            />
+                            <HomePage />
                         </Route>
                     </Switch>
                 </BrowserRouter>
