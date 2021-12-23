d4c80c5883505a851e2e69e5c64ea06d636e16b2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-23 14:10:47 +0100

    [refactor] Change handleClick by onClick

--- src/client/components/header/header.js
+++ src/client/components/header/header.js
@@ -9,39 +9,33 @@ import Button from "../button/button";
 import {faBars, faTimes} from "@fortawesome/free-solid-svg-icons";
 
 class Header extends Component {
-    render() {
-        return (
-            <header
-                className={
-                    this.props.isMenuOpen ? "header" : "header gradient"
-                }>
-                {this.props.fullName ? (
-                    <h1 className={"header__title"}>
-                        <NavLink
-                            to={"/"}
-                            exact={true}
-                            handleClick={this.props.closeMenu}
-                            className={"header__link"}>
-                            {this.props.fullName.firstName}
-                            <br />
-                            {this.props.fullName.lastName}
-                        </NavLink>
-                    </h1>
-                ) : null}
-                {/* Button to open/close the menu */}
-                <Button
-                    function={this.props.toggleMenu}
-                    icon={this.props.isMenuOpen ? faTimes : faBars}
-                    alt={
-                        this.props.isMenuOpen
-                            ? "Fermer le menu"
-                            : "Ouvrir le menu"
-                    }
-                    class={"button__menu"}
-                />
-            </header>
-        );
-    }
+	render() {
+		return (
+			<header className={this.props.isMenuOpen ? "header" : "header gradient"}>
+				{this.props.fullName
+					? <h1 className={"header__title"}>
+						<NavLink
+							to={"/"}
+							exact={true}
+							onClick={this.props.closeMenu}
+							className={"header__link"}>
+							{this.props.fullName.firstName}
+							<br />
+							{this.props.fullName.lastName}
+						</NavLink>
+					</h1>
+					: null
+				}
+				{/* Button to open/close the menu */}
+				<Button
+					function={this.props.toggleMenu}
+					icon={this.props.isMenuOpen ? faTimes : faBars}
+					alt={this.props.isMenuOpen ? "Fermer le menu" : "Ouvrir le menu"}
+					class={"button__menu"}
+				/>
+			</header>
+		);
+	}
 }
 
 export default hot(Header);
