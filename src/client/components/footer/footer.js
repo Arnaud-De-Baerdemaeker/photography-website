6549ddb63506b7c44d8913429c84bf587c568e2e
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-24 15:54:40 +0100

    [feat] Add Instagram link
    - Removed the light/dark control
    - Removed the material icons to use Font Awesome
    - Reworked the general structure

--- src/client/components/footer/footer.js
+++ src/client/components/footer/footer.js
@@ -5,96 +5,86 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {faInstagram, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
 
 class Footer extends Component {
-    render() {
-        const year = new Date();
+	render() {
+		const year = new Date();
 
-        return (
-            <footer
-                className={
-                    this.props.lightModeOn ? "footer" : "footer--dark-mode"
-                }>
-                <div className={"footer__social-container"}>
-                    <ul
-                        className={
-                            this.props.lightModeOn
-                                ? "footer__links"
-                                : "footer__links--dark-mode"
-                        }>
-                        <li className={"footer__network"}>
-                            <a
-                                href={"mailto:de.baerdemaeker.arnaud@gmail.com"}
-                                target={"_blank"}
-                                rel={"noreferrer noopener"}
-                                className={"footer__link"}>
-                                <i
-                                    className={
-                                        this.props.lightModeOn
-                                            ? "material-icons"
-                                            : "material-icons dark-mode"
-                                    }
-                                    alt={"Email"}
-                                    title={"Contactez-moi"}>
-                                    {"email"}
-                                </i>
-                            </a>
-                        </li>
-                        <li className={"footer__network"}>
-                            <a
-                                href={
-                                    "https://www.linkedin.com/in/arnaud-de-baerdemaeker/"
-                                }
-                                target={"_blank"}
-                                rel={"noreferrer noopener"}
-                                className={"footer__link"}>
-                                <FontAwesomeIcon
-                                    icon={faLinkedin}
-                                    alt={"LinkedIn"}
-                                    title={"Mon profil LinkedIn"}
-                                    className={
-                                        this.props.lightModeOn
-                                            ? "footer__icon"
-                                            : "footer__icon--dark-mode"
-                                    }
-                                />
-                            </a>
-                        </li>
-                        <li className={"footer__network"}>
-                            <a
-                                href={
-                                    "https://github.com/Arnaud-De-Baerdemaeker"
-                                }
-                                target={"_blank"}
-                                rel={"noreferrer noopener"}
-                                className={"footer__link"}>
-                                <FontAwesomeIcon
-                                    icon={faGithub}
-                                    alt={"GitHub"}
-                                    title={"Mon travail sur GitHub"}
-                                    className={
-                                        this.props.lightModeOn
-                                            ? "footer__icon"
-                                            : "footer__icon--dark-mode"
-                                    }
-                                />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className={"footer__credits-container"}>
-                    <p className={"footer__credits"}>
-                        {`${year.getUTCFullYear()} Arnaud De Baerdemaeker`}
-                    </p>
-                    <p className={"footer__credits"}>
-                        {"Tous droits réservés"}
-                    </p>
-                    <p className={"footer__credits"}>{"Version 1.1.2"}</p>
-                </div>
-            </footer>
-        );
-    }
+		return (
+			<footer className={"footer"}>
+				<div className={"footer__social-networks"}>
+					<ul className={"footer__links"}>
+						<li className={"footer__network"}>
+							<a
+								href={"mailto:de.baerdemaeker.arnaud@gmail.com"}
+								target={"_blank"}
+								rel={"noreferrer noopener"}
+								className={"footer__link"}>
+								<FontAwesomeIcon
+									icon={faEnvelope}
+									alt={"Email"}
+									title={"Contactez-moi"}
+									className={"footer__icon"}
+								/>
+							</a>
+						</li>
+						<li className={"footer__network"}>
+							<a
+								href={"https://www.instagram.com/los.2102/"}
+								target={"_blank"}
+								rel={"noreferrer noopener"}
+								className={"footer__link"}>
+								<FontAwesomeIcon
+									icon={faInstagram}
+									alt={"Instagram"}
+									title={"Mon profil Instagram"}
+									className={"footer__icon"}
+								/>
+							</a>
+						</li>
+						<li className={"footer__network"}>
+							<a
+								href={"https://www.linkedin.com/in/arnaud-de-baerdemaeker/"}
+								target={"_blank"}
+								rel={"noreferrer noopener"}
+								className={"footer__link"}>
+								<FontAwesomeIcon
+									icon={faLinkedin}
+									alt={"LinkedIn"}
+									title={"Mon profil LinkedIn"}
+									className={"footer__icon"}
+								/>
+							</a>
+						</li>
+						<li className={"footer__network"}>
+							<a
+								href={"https://github.com/Arnaud-De-Baerdemaeker"}
+								target={"_blank"}
+								rel={"noreferrer noopener"}
+								className={"footer__link"}>
+								<FontAwesomeIcon
+									icon={faGithub}
+									alt={"GitHub"}
+									title={"Mon travail sur GitHub"}
+									className={"footer__icon"}
+								/>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className={"footer__credits-container"}>
+					<p className={"footer__credits"}>
+						{`${year.getUTCFullYear()} - Arnaud De Baerdemaeker`}
+						<br />
+						{"Tous droits réservés"}
+					</p>
+					<p className={"footer__credits"}>{"Version 2.0.0"}</p>
+				</div>
+			</footer>
+		);
+	}
 }
 
 export default hot(Footer);
