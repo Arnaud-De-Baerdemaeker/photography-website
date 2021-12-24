e4d9fedd7e797a165291c7919b6c8eca28e3a94a
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-24 16:12:31 +0100

    [refactor] Rework the navigation component
    - Removed the light/dark control, local and online adresses and the function to determine the class name
    - Added event listeners to close the menu when scrolling
    - Removed the icons

--- src/client/components/navigation/navigation.js
+++ src/client/components/navigation/navigation.js
@@ -7,60 +7,49 @@ import {hot} from "react-hot-loader/root";
 import {NavLink} from "react-router-dom";
 
 class Navigation extends Component {
-    render() {
-        const location = window.location.toString();
-        const local = "http://192.168.0.10:4000/";
-        const online = "https://arnaud-de-baerdemaeker.netlify.app/";
-        const lightModeOn = this.props.lightModeOn;
+	componentDidMount() {
+		// Listens to when the user is scrolling and closeMenu the menu if it is open
+		window.addEventListener("scroll", this.props.closeMenu);
+	}
 
-        function setNavBarClassName() {
-            if (location === local || location === online) {
-                return "menu__container--homepage";
-            }
-            if (lightModeOn === true) {
-                return "menu__container--other-pages";
-            }
-            return "menu__container--other-pages dark-mode";
-        }
+	componentWillUnmount() {
+		window.removeEventListener("scroll", this.props.closeMenu);
+	}
 
-        return (
-            <nav className={setNavBarClassName()}>
-                <ul className={"menu__list"}>
-                    <li className={"menu__list-item"}>
-                        <NavLink
-                            to={"/"}
-                            exact={true} // Allows to exactly match the url set in the "to" attribute
-                            activeClassName={"menu__link--active"} // Class name that is active only when the page is active
-                            className={"menu__link"}>
-                            <i
-                                className={
-                                    this.props.lightModeOn
-                                        ? "material-icons"
-                                        : "material-icons dark-mode"
-                                }>
-                                {"home"}
-                            </i>
-                        </NavLink>
-                    </li>
-                    <li className={"menu__list-item"}>
-                        <NavLink
-                            to={"/gallery"}
-                            activeClassName={"menu__link--active"} // Class name that is active only when the page is active
-                            className={"menu__link"}>
-                            <i
-                                className={
-                                    this.props.lightModeOn
-                                        ? "material-icons"
-                                        : "material-icons dark-mode"
-                                }>
-                                {"photo_library"}
-                            </i>
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+	render() {
+		return (
+			<nav
+				onScroll={this.props.closeMenu}
+				className={this.props.isMenuOpen ? "menu" : "menu hidden"}>
+				<ul className={"menu__list"}>
+					<li className={"menu__list-item"}>
+						<NavLink
+							to={"/"}
+							// Allows to exactly match the url set in the "to" attribute
+							exact={true}
+							onClick={this.props.toggleMenu}
+							id={"home"}
+							// Class name that is active only when the page is active
+							activeClassName={"menu__link--active"}
+							className={"menu__link"}>
+							{"Accueil"}
+						</NavLink>
+					</li>
+					<li className={"menu__list-item"}>
+						<NavLink
+							to={"/gallery"}
+							onClick={this.props.toggleMenu}
+							id={"gallery"}
+							// Class name that is active only when the page is active
+							activeClassName={"menu__link--active"}
+							className={"menu__link"}>
+							{"Galerie"}
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
 }
 
 export default hot(Navigation);
