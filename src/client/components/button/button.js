a8aafb3eaff5b708db4c619d0af55a9d4c33d9ad
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-24 15:52:30 +0100

    [refactor] Rework the button component to adapt it to several situations

--- src/client/components/button/button.js
+++ src/client/components/button/button.js
@@ -1,49 +1,22 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
-import Modal from "../modal/modal";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class Button extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            modalIsOpen: false,
-        };
-        this.handleOnClick = this.handleOnClick.bind(this);
-    }
-
-    // Function that changes the boolean state each time it is called
-    handleOnClick() {
-        this.setState(state => ({
-            modalIsOpen: !state.modalIsOpen,
-        }));
-    }
-
-    render() {
-        return (
-            <>
-                <div className={"button__container"}>
-                    <button
-                        type={"button"}
-                        onClick={this.handleOnClick}
-                        className={
-                            this.props.lightModeOn
-                                ? "button__item"
-                                : "button__item--dark-mode"
-                        }>
-                        {"Voir"}
-                    </button>
-                </div>
-                {/* Modal that is active only when the user clicks on the thumbnail */}
-                {this.state.modalIsOpen ? (
-                    <Modal
-                        url={this.props.fullImages}
-                        onClick={this.handleOnClick}
-                        lightModeOn={this.props.lightModeOn}
-                    />
-                ) : null}
-            </>
-        );
-    }
+	render() {
+		return (
+			<button
+				type={"button"}
+				onClick={this.props.function}
+				className={this.props.class}>
+				<FontAwesomeIcon
+					icon={this.props.icon}
+					alt={this.props.alt}
+					className={"button__icon"}
+				/>
+			</button>
+		);
+	}
 }
 
 export default hot(Button);
