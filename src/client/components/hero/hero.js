eeb3f8349f48f8db7818f7300058b3e228312746
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-24 16:05:03 +0100

    [refactor] Rework the hero component
    Built conditions to know when to show the main title and the title of the gallery page and their subtitles

--- src/client/components/hero/hero.js
+++ src/client/components/hero/hero.js
@@ -2,26 +2,51 @@ import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 
 class Hero extends Component {
-    render() {
-        return (
-            <div className={"hero"}>
-                <div className={"hero__back-filter"}>
-                    <div className={"hero__gradient"}>
-                        <h1 className={"hero__main-title"}>
-                            {"ARNAUD"}
-                            <br />
-                            {"DE BAERDEMAEKER"}
-                        </h1>
-                        <h2 className={"hero__subtitle"}>
-                            {"Développeur web"}
-                            <br />
-                            {"Amateur de photographie"}
-                        </h2>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		const title = [];
+		const subtitle = [];
+
+		if (this.props.fullName) {
+			title.push(
+				<h1 key={this.props.fullName} className={"hero__title"}>
+					{this.props.fullName.firstName}
+					<br />
+					{this.props.fullName.lastName}
+				</h1>
+			);
+		}
+		else {
+			title.push(
+				<h1 key={this.props.title} className={"hero__title"}>{this.props.title}</h1>
+			);
+		}
+
+		if (typeof this.props.subtitle === "object") {
+			subtitle.push(
+				<h2 key={this.props.subtitle} className={"hero__subtitle"}>
+					{this.props.subtitle.job}
+					<br />
+					{this.props.subtitle.hobby}
+				</h2>
+			);
+		}
+		else {
+			subtitle.push(
+				<h2 key={this.props.subtitle} className={"hero__subtitle"}>{this.props.subtitle}</h2>
+			);
+		}
+
+		return (
+			<div className={"hero"}>
+				<div className={"hero__back-filter"}>
+					<div className={"hero__gradient"}>
+						{title}
+						{subtitle}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default hot(Hero);
