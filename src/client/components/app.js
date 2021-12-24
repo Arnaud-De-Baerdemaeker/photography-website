e0d93e2bd4e7ae9a0a1fb8aa8b6ecf223d4e766a
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-24 16:17:06 +0100

    [refactor] Rework the app component
    - Set the controls for the menu
    - Set the text for the hero of the homepage
    - Removed the routes to the years' list
    - Removed the titles and photos props
    - Sent the menu controls via props

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -8,91 +8,84 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 import {Helmet} from "react-helmet";
 import HomePage from "./homepage/homepage";
 import Gallery from "./gallery-page/gallery";
-import PhotosList from "./photos-list/photos-list";
-import data from "./../JSON/metadata.json";
+import photos from "./../JSON/metadata.json";
 
 class App extends Component {
-    render() {
-        return (
-            <>
-                <Helmet>
-                    <meta
-                        name={"viewport"}
-                        content={"width=device-width, initial-scale=1.0"}
-                    />
-                    <link
-                        rel={"preconnect"}
-                        href={"https://fonts.googleapis.com"}
-                    />
-                    <link
-                        rel={"preconnect"}
-                        href={"https://fonts.gstatic.com"}
-                        crossOrigin
-                    />
-                    <link
-                        href={
-                            "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Comfortaa:wght@300;400;500;600;700&family=DM+Sans:wght@400;500;700&display=swap"
-                        }
-                        rel={"stylesheet"}
-                    />
-                    <title>{"Arnaud De Baerdemaeker"}</title>
-                </Helmet>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path={"/gallery/2014"}>
-                            <PhotosList album={data[0].album_2014} />
-                        </Route>
-                        <Route exact path={"/gallery/2015"}>
-                            <PhotosList album={data[1].album_2015} />
-                        </Route>
-                        <Route exact path={"/gallery/2016"}>
-                            <PhotosList album={data[2].album_2016} />
-                        </Route>
-                        <Route exact path={"/gallery/2017"}>
-                            <PhotosList album={data[3].album_2017} />
-                        </Route>
-                        <Route exact path={"/gallery/2018"}>
-                            <PhotosList album={data[4].album_2018} />
-                        </Route>
-                        <Route exact path={"/gallery/2019"}>
-                            <PhotosList album={data[5].album_2019} />
-                        </Route>
-                        <Route exact path={"/gallery"}>
-                            <Gallery
-                                title2014={data[0].album_2014.title}
-                                thumbnail2014={
-                                    data[0].album_2014.photos[0]["576p"]
-                                }
-                                title2015={data[1].album_2015.title}
-                                thumbnail2015={
-                                    data[1].album_2015.photos[0]["576p"]
-                                }
-                                title2016={data[2].album_2016.title}
-                                thumbnail2016={
-                                    data[2].album_2016.photos[0]["576p"]
-                                }
-                                title2017={data[3].album_2017.title}
-                                thumbnail2017={
-                                    data[3].album_2017.photos[0]["576p"]
-                                }
-                                title2018={data[4].album_2018.title}
-                                thumbnail2018={
-                                    data[4].album_2018.photos[0]["576p"]
-                                }
-                                title2019={data[5].album_2019.title}
-                                thumbnail2019={
-                                    data[5].album_2019.photos[0]["576p"]
-                                }
-                            />
-                        </Route>
-                        <Route exact path={"/"}>
-                            <HomePage />
-                        </Route>
-                    </Switch>
-                </BrowserRouter>
-            </>
-        );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isMenuOpen: false
+		};
+		this.toggleMenu = this.toggleMenu.bind(this);
+		this.closeMenu = this.closeMenu.bind(this);
+	}
+
+	toggleMenu() {
+		this.setState(state => ({
+			isMenuOpen: !state.isMenuOpen
+		}));
+	}
+
+	closeMenu() {
+		if (this.state.isMenuOpen === true) {
+			this.setState({
+				isMenuOpen: false
+			});
+		}
+	}
+
+	render() {
+		const fullName = {
+			firstName: "Arnaud",
+			lastName: "De Baerdemaeker"
+		};
+
+		return (
+			<>
+				<Helmet>
+					<meta
+						name={"viewport"}
+						content={"width=device-width, initial-scale=1.0"}
+					/>
+					<link
+						rel={"preconnect"}
+						href={"https://fonts.googleapis.com"}
+					/>
+					<link
+						rel={"preconnect"}
+						href={"https://fonts.gstatic.com"}
+						crossOrigin
+					/>
+					<link
+						href={"https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Comfortaa:wght@300;400;500;600;700&family=DM+Sans:wght@400;500;700&display=swap"}
+						rel={"stylesheet"}
+					/>
+					<title>{"Arnaud De Baerdemaeker"}</title>
+				</Helmet>
+				<BrowserRouter>
+					<Switch>
+						<Route exact path={"/gallery"}>
+							<Gallery
+								fullName={fullName}
+								isMenuOpen={this.state.isMenuOpen}
+								toggleMenu={this.toggleMenu}
+								closeMenu={this.closeMenu}
+								photos={photos}
+							/>
+						</Route>
+						<Route exact path={"/"}>
+							<HomePage
+								fullName={fullName}
+								isMenuOpen={this.state.isMenuOpen}
+								toggleMenu={this.toggleMenu}
+								closeMenu={this.closeMenu}
+							/>
+						</Route>
+					</Switch>
+				</BrowserRouter>
+			</>
+		);
+	}
 }
 
 export default hot(App);
