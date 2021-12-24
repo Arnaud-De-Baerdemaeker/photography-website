29d70d6a9d4814b6175ac6f73b7eeaf4616ce587
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2021-12-24 16:14:19 +0100

    [refactor] Rework the photos-cards component
    - Removed the light/dark control
    - Removed all the details about the photos
    - Set the control of the modal on the image itself
    - Calls the modal from here

--- src/client/components/photos-cards/photos-cards.js
+++ src/client/components/photos-cards/photos-cards.js
@@ -5,82 +5,43 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 import Image from "../image/image";
-import Button from "../button/button.js";
+import Modal from "../modal/modal";
 
 class PhotosCards extends Component {
-    render() {
-        const images = {
-            hd: this.props.hd,
-            //uhd: this.props.uhd,
-        };
+	constructor(props) {
+		super(props);
+		this.state = {
+			isModalOpen: false
+		}
+		this.toggleModal = this.toggleModal.bind(this);
+	}
 
-        return (
-            <div
-                className={
-                    this.props.lightModeOn
-                        ? "photos-cards__card"
-                        : "photos-cards__card--dark-mode"
-                }>
-                <div className={"photos-cards__titles"}>
-                    <h3
-                        className={
-                            this.props.lightModeOn
-                                ? "photos-cards__mainTitle"
-                                : "photos-cards__mainTitle--dark-mode"
-                        }>
-                        {this.props.name}
-                    </h3>
-                    <h4
-                        className={
-                            this.props.lightModeOn
-                                ? "photos-cards__subtitle"
-                                : "photos-cards__subtitle--dark-mode"
-                        }>
-                        {this.props.place}
-                    </h4>
-                </div>
-                <figure className={"photos-cards__image-container"}>
-                    <picture>
-                        <Image
-                            url={this.props.sd}
-                            alt={this.props.name}
-                            title={this.props.name}
-                            class={"photos-cards__image"}
-                        />
-                    </picture>
-                    <figcaption className={"photos-cards__caption"}>
-                        {this.props.author}
-                    </figcaption>
-                </figure>
-                <div className={"photos-cards__metadata"}>
-                    <ul className={"photos-cards__list"}>
-                        <li className={"photos-cards__list-item"}>
-                            {`${this.props.day} ${this.props.date} ${this.props.month} ${this.props.year}, ${this.props.hours}:${this.props.minutes}`}
-                        </li>
-                        <li className={"photos-cards__list-item"}>
-                            {`Ouverture : f/${this.props.aperture}`}
-                        </li>
-                        <li className={"photos-cards__list-item"}>
-                            {`Vitesse d'obturation : ${this.props.shutter_speed}`}
-                        </li>
-                        <li className={"photos-cards__list-item"}>
-                            {`Distance focale : ${this.props.focal_length} mm`}
-                        </li>
-                        <li className={"photos-cards__list-item"}>
-                            {`ISO : ${this.props.iso}`}
-                        </li>
-                        <li className={"photos-cards__list-item"}>
-                            {this.props.camera}
-                        </li>
-                    </ul>
-                </div>
-                <Button
-                    fullImages={images}
-                    lightModeOn={this.props.lightModeOn}
-                />
-            </div>
-        );
-    }
+	toggleModal() {
+		this.setState(state => ({
+			isModalOpen: !state.isModalOpen
+		}));
+	}
+
+	render() {
+		return (
+			<>
+				<div
+					onClick={this.toggleModal}
+					className={"photos-cards"}>
+					<Image
+						url={this.props.sd}
+						name={this.props.name}
+						place={this.props.place}
+					/>
+				</div>
+				<Modal
+					hd={this.props.hd}
+					isModalOpen={this.state.isModalOpen}
+					toggleModal={this.toggleModal}
+				/>
+			</>
+		);
+	}
 }
 
 export default hot(PhotosCards);
