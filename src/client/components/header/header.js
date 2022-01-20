65648b55400361dd39d5138fdb315d8bba43dced
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-01-20 16:31:57 +0100

    [refactor] Build the menu icon manually

--- src/client/components/header/header.js
+++ src/client/components/header/header.js
@@ -4,35 +4,47 @@
 
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
-import {NavLink} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
+
 import Button from "../button/button";
 
 class Header extends Component {
 	render() {
 		return (
-			<header className={this.props.isMenuOpen ? this.props.class : this.props.class + " gradient"}>
-				{this.props.fullName
-					? <h1 className={"header__title"}>
-						<NavLink
-							to={"/"}
-							exact={true}
-							onClick={this.props.closeMenu}
-							className={"header__link"}>
-							{this.props.fullName.firstName}
-							<br />
-							{this.props.fullName.lastName}
-						</NavLink>
-					</h1>
-					: null
-				}
+			<header className={"header"}>
+				<h1 className={"header__name"}>
+					<Link
+						to={"/"}
+						onClick={this.props.closeMenu}
+						className={"name__link"}
+					>
+						{"Arnaud"}
+						<br />
+						{"De Baerdemaeker"}
+					</Link>
+				</h1>
 				<Button
 					function={this.props.toggleMenu}
-					alt={this.props.isMenuOpen ? "Fermer le menu" : "Ouvrir le menu"}
-					class={this.props.isMenuOpen ? "button__close" : "button__menu"}
+					alt={this.props.isMenuOpen
+						? "Fermer le menu"
+						: "Ouvrir le menu"
+					}
+					class={this.props.isMenuOpen
+						? [
+							"button__top-bar--close",
+							"button__middle-bar--close",
+							"button__bottom-bar--close"
+						]
+						: [
+							"button__top-bar",
+							"button__middle-bar",
+							"button__bottom-bar"
+						]
+					}
 				/>
 			</header>
 		);
 	}
 }
 
-export default hot(Header);
+export default hot(withRouter(Header));
