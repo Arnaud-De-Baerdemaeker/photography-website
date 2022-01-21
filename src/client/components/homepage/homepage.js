2a9d1ae94932eb1dc957026ca730f91e66a4ab54
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-01-21 16:49:59 +0100

    [cleanup] Remove unused import

--- src/client/components/homepage/homepage.js
+++ src/client/components/homepage/homepage.js
@@ -6,7 +6,6 @@ import React, {Component} from "react";
 import {withRouter} from "react-router-dom";
 import {hot} from "react-hot-loader/root";
 
-// import Hero from "../hero/hero";
 import Image from "../image/image";
 import myPhoto from "../../images/moi.png";
 
