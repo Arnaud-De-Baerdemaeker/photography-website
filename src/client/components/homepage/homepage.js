49d59d6e9c9da9d285ac3add19fd6cd5172df6a6
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-01-27 10:43:13 +0100

    [refactor] Put a line-break in the text

--- src/client/components/homepage/homepage.js
+++ src/client/components/homepage/homepage.js
@@ -31,7 +31,7 @@ class HomePage extends Component {
 						/>
 					</div>
 					<div className={"homepage__introduction"}>
-						<p className={"introduction__paragraph"}>{"Bonjour, je m'appelle Arnaud ! Développeur web résidant en Belgique, dans la ville de Liège, et amateur de photographie."}</p>
+						<p className={"introduction__paragraph"}>{"Bonjour, je m'appelle Arnaud !"}<br />{"Développeur web résidant en Belgique, dans la ville de Liège, et amateur de photographie."}</p>
 						<p className={"introduction__paragraph"}>{"Je suis arrivé dans le domaine du web un peu par hasard, et de fil en aiguille, j'ai été captivé. Mon apprentissage a commencé en tant qu'auto-didacte, internet étant une formidable source de tutoriels. Par la suite, j'ai tenu à officialiser mes acquis avec des formations. Aujourd'hui, je suis heureux d'avoir pu en faire mon métier."}</p>
 						<p className={"introduction__paragraph"}>{"Vers la fin de ma formation, l'idée de me créer un petit portfolio s'est imposée. L'objectif était multiple : mettre en pratique les connaissances apprises, me présenter, et montrer les fruits de mon intérêt pour la photographie."}</p>
 						<p className={"introduction__paragraph"}>{"Bien que touche-à-tout avec les technologies du web, j'affectionne particulièrement le fait de développer ma créativité pour construire des designs, rédiger des lignes de code, admirer leur logique, et les voir prendre vie dans le navigateur. Réfléchir aux couleurs à utiliser, à la disposition des différents éléments, aux formes, à l'expérience utilisateur, sont autant de problématiques que j'apprécie de résoudre soigneusement."}</p>
