2b05515831e9fa92d94b410548ca986c6d58295f
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-01-27 12:29:07 +0100

    [refactor] Set the name as a component

---
+++ src/client/components/name/name.js
@@ -0,0 +1,27 @@
+// Photography website
+// Started on July 2020
+// By Arnaud De Baerdemaeker
+
+import React, {Component} from "react";
+import {hot} from "react-hot-loader/root";
+import {Link} from "react-router-dom";
+
+class Name extends Component {
+    render() {
+        return (
+            <h1 className={this.props.class}>
+				<Link
+					to={"/"}
+					onClick={this.props.closeMenu}
+					className={"name__link"}
+				>
+					{"Arnaud"}
+					<br />
+					{"De Baerdemaeker"}
+				</Link>
+			</h1>
+        );
+    }
+}
+
+export default hot(Name);
