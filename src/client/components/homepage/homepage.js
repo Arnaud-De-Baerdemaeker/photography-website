0fde4d75383cca8da53817a3ff1d0f77a9d5c931
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-02-03 16:39:04 +0100

    [refactor] Rework the description and add titles

--- src/client/components/homepage/homepage.js
+++ src/client/components/homepage/homepage.js
@@ -31,16 +31,22 @@ class HomePage extends Component {
 						<Image
 							url={myPhoto}
 							alt={"Photo de moi"}
-							class={"my-photo__image"}
+							class={"image__portrait"}
 						/>
+						<p className={"homepage__introduction"}>{"Bonjour, je m'appelle Arnaud !"}<br />{"Développeur web résidant en Belgique, dans la ville de Liège, et amateur de photographie."}</p>
 					</div>
-					<div className={"homepage__introduction"}>
-						<p className={"introduction__paragraph"}>{"Bonjour, je m'appelle Arnaud !"}<br />{"Développeur web résidant en Belgique, dans la ville de Liège, et amateur de photographie."}</p>
-						<p className={"introduction__paragraph"}>{"Je suis arrivé dans le domaine du web un peu par hasard, et de fil en aiguille, j'ai été captivé. Mon apprentissage a commencé en tant qu'auto-didacte, internet étant une formidable source de tutoriels. Par la suite, j'ai tenu à officialiser mes acquis avec des formations. Aujourd'hui, je suis heureux d'avoir pu en faire mon métier."}</p>
-						<p className={"introduction__paragraph"}>{"Vers la fin de ma formation, l'idée de me créer un petit portfolio s'est imposée. L'objectif était multiple : mettre en pratique les connaissances apprises, me présenter, et montrer les fruits de mon intérêt pour la photographie."}</p>
-						<p className={"introduction__paragraph"}>{"Bien que touche-à-tout avec les technologies du web, j'affectionne particulièrement le fait de développer ma créativité pour construire des designs, rédiger des lignes de code, admirer leur logique, et les voir prendre vie dans le navigateur. Réfléchir aux couleurs à utiliser, à la disposition des différents éléments, aux formes, à l'expérience utilisateur, sont autant de problématiques que j'apprécie de résoudre soigneusement."}</p>
-						<p className={"introduction__paragraph"}>{"De la même manière qu'avec le web, je me suis intéressé à la photographie et en ait acquis les bases par moi-même. Ce qui me plait, c'est d'immortaliser l'instant, montrer la beauté dans des choses ordinaires. La nature sous toutes ses formes, les architectures, et les photos rapprochées sont les types de sujets qui m'inspirent le plus."}</p>
-						<p className={"introduction__paragraph"}>{"Durant mon temps libre, je prends plaisir à faire du vélo, jouer aux jeux vidéo, lire, ou tout simplement me balader entre-autres."}</p>
+					<div className={"homepage__description"}>
+						<div className={"homepage__section"}>
+							<h3 className={"homepage__title"}>{" Le développement web"}</h3>
+							<p className={"homepage__paragraph"}>{"Je suis arrivé dans le domaine du web un peu par hasard, et de fil en aiguille, j'ai été captivé. Mon apprentissage a commencé en tant qu'auto-didacte, internet étant une formidable source de tutoriels. Par la suite, j'ai tenu à officialiser mes acquis avec des formations. Aujourd'hui, je suis heureux d'avoir pu en faire mon métier."}</p>
+							<p className={"homepage__paragraph"}>{"Vers la fin de ma formation, l'idée de me créer un petit portfolio s'est imposée. L'objectif était multiple : mettre en pratique les connaissances apprises, me présenter, et montrer les fruits de mon intérêt pour la photographie."}</p>
+							<p className={"homepage__paragraph"}>{"Bien que touche-à-tout avec les technologies du web, j'affectionne particulièrement le fait de développer ma créativité pour construire des designs, rédiger des lignes de code, admirer leur logique, et les voir prendre vie dans le navigateur. Réfléchir aux couleurs à utiliser, à la disposition des différents éléments, aux formes, à l'expérience utilisateur, sont autant de problématiques que j'apprécie de résoudre soigneusement."}</p>
+						</div>
+						<div className={"homepage__section"}>
+							<h3 className={"homepage__title"}>{"La photographie"}</h3>
+							<p className={"homepage__paragraph"}>{"De la même manière qu'avec le web, je me suis intéressé à la photographie et en ait acquis les bases par moi-même. Ce qui me plait, c'est d'immortaliser l'instant, montrer la beauté dans des choses ordinaires. La nature sous toutes ses formes, les architectures, et les photos rapprochées sont les types de sujets qui m'inspirent le plus."}</p>
+							<p className={"homepage__paragraph"}>{"Durant mon temps libre, je prends plaisir à faire du vélo, jouer aux jeux vidéo, lire, ou tout simplement me balader entre-autres."}</p>
+						</div>
 					</div>
 				</main>
 			</>
