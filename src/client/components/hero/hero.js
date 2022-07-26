7b612b2c9f95ba896add03681b4c48ac4b74984e
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-07-26 16:31:52 +0200

    [refactor] Set the conditionnal hero display in a function

--- src/client/components/hero/hero.js
+++ src/client/components/hero/hero.js
@@ -13,24 +13,36 @@ class Hero extends Component {
 		clearInterval(this.animate);
 	}
 
+	filterRoute() {
+		console.log(this.props.location.pathname);
+		switch(this.props.location.pathname) {
+			case "/":
+				return <>
+					<span className={"title__job-part1"}>{this.props.jobPart1}</span>
+					<span className={"title__job-part2"}>{this.props.jobPart2}</span>
+					<span className={"title__ampersand"}>{this.props.ampersand}</span>
+					<span className={"title__hobby-part1"}>{this.props.hobbyPart1}</span>
+					<span className={"title__hobby-part2"}>{this.props.hobbyPart2}</span>
+				</>
+			case "/galerie":
+				return <>
+					<span className={"title__page"}>{this.props.title}</span>
+					<span className={"title__intro"}>{this.props.subtitle}</span>
+				</>
+			default:
+				return <>
+					<span className={"title__page"}>{this.props.titlePart1}</span>
+					<span className={"title__intro"}>{this.props.subtitle}</span>
+				</>
+		}
+	}
+
 	render() {
 		return (
 			<div className={"hero " + this.props.containerClass}>
 				<div className={"hero__back-filter"}>
 					<h2 className={this.props.titleClass}>
-						{this.props.location.pathname === "/"
-							? <>
-								<span className={"title__job-part1"}>{this.props.title.job.part1}</span>
-								<span className={"title__job-part2"}>{this.props.title.job.part2}</span>
-								<span className={"title__ampersand"}>{this.props.title.ampersand}</span>
-								<span className={"title__hobby-part1"}>{this.props.title.hobby.part1}</span>
-								<span className={"title__hobby-part2"}>{this.props.title.hobby.part2}</span>
-							</>
-							: <>
-								<span className={"title__page"}>{this.props.title.title}</span>
-								<span className={"title__intro"}>{this.props.title.subtitle}</span>
-							</>
-						}
+						{this.filterRoute()}
 					</h2>
 					<div className={"hero__move-down-icon"}>
 						<div className={"hero__arrow"}></div>
