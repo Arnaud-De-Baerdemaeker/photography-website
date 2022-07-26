de02c1dca2851b9fab738b7ad1897a8e96587d8d
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-07-26 16:22:02 +0200

    [feat] Pass the props to the Hero component

--- src/client/components/error404/error404.js
+++ src/client/components/error404/error404.js
@@ -5,18 +5,20 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader/root";
 
-import Header from "../header/header";
-import Footer from "../footer/footer";
+import Hero from "../hero/hero";
 
 class Error404 extends Component {
-    render() {
-        return(
-            <>
-                <Header />
-                <Footer />
-            </>
-        );
-    }
+	render() {
+		return(
+			<Hero
+				titlePart1={"Oups !"}
+				titlePart2={"C'est un 404"}
+				subtitle={"La page que vous recherchez n'existe pas"}
+				containerClass={"hero__404"}
+				titleClass={"hero__title--404"}
+			/>
+		);
+	}
 }
 
 export default hot(Error404);
