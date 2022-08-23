2e213ec9a1337560f510fd549d06a1fc85abf36b
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-08-23 12:03:39 +0200

    [refactor] Remove react-helmet and move <head> details in HTML file

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -4,15 +4,14 @@
 
 import React, {Component, lazy, Suspense} from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
-import {Helmet} from "react-helmet";
 
 import Loading from "./loading-screen/loading-screen.js";
-const Header = lazy(() => import("./header/header"));
-const Navigation = lazy(() => import("./navigation/navigation"));
+import Header from "./header/header";
+import Navigation from "./navigation/navigation";
 const HomePage = lazy(() => import("./homepage/homepage"));
 const Gallery = lazy(() => import("./gallery-page/gallery"));
 const Error404 = lazy(() => import("./error404/error404"));
-const Footer = lazy(() => import("./footer/footer"));
+import Footer from "./footer/footer";
 
 class App extends Component {
 	constructor(props) {
@@ -86,72 +85,50 @@ class App extends Component {
 
 	render() {
 		return (
-			<>
-				<Helmet>
-					<meta
-						name={"viewport"}
-						content={"width=device-width, initial-scale=1.0"}
-					/>
-					<link
-						rel={"preconnect"}
-						href={"https://fonts.googleapis.com"}
-					/>
-					<link
-						rel={"preconnect"}
-						href={"https://fonts.gstatic.com"}
-						crossOrigin
-					/>
-					<link
-						href={"https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Crimson+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=DM+Sans:wght@400;500;700&display=swap"}
-						rel={"stylesheet"}
-					/>
-					{/* <title>{}</title> */}
-				</Helmet>
+			<BrowserRouter>
+				<Header
+					isMenuOpen={this.state.isMenuOpen}
+					toggleMenu={this.toggleMenu}
+					closeMenu={this.closeMenu}
+				/>
+				<Navigation
+					isMenuOpen={this.state.isMenuOpen}
+					toggleMenu={this.toggleMenu}
+					closeMenu={this.closeMenu}
+				/>
 				<Suspense fallback={<Loading />}>
-					<BrowserRouter>
-						<Header
-							isMenuOpen={this.state.isMenuOpen}
-							toggleMenu={this.toggleMenu}
-							closeMenu={this.closeMenu}
-						/>
-						<Navigation
-							isMenuOpen={this.state.isMenuOpen}
-							toggleMenu={this.toggleMenu}
-							closeMenu={this.closeMenu}
-						/>
-						<Switch>
-							<Route
-								exact
-								path={"/galerie"}
-							>
-								<Gallery
-									isMenuOpen={this.state.isMenuOpen}
-									toggleMenu={this.toggleMenu}
-									closeMenu={this.closeMenu}
-									applyHideClass={this.applyHideClass}
-									transferOnScroll={this.transferOnScroll}
-								/>
-							</Route>
-							<Route
-								exact
-								path={"/"}
-							>
-								<HomePage
-									isMenuOpen={this.state.isMenuOpen}
-									toggleMenu={this.toggleMenu}
-									closeMenu={this.closeMenu}
-									applyHideClass={this.applyHideClass}
-									transferOnScroll={this.transferOnScroll}
-								/>
-							</Route>
-							<Route path="*">
-								<Error404 />
-							</Route>
-						</Switch>
-						<Footer />
-					</BrowserRouter>
+					<Switch>
+						<Route
+							exact
+							path={"/galerie"}
+						>
+							<Gallery
+								isMenuOpen={this.state.isMenuOpen}
+								toggleMenu={this.toggleMenu}
+								closeMenu={this.closeMenu}
+								applyHideClass={this.applyHideClass}
+								transferOnScroll={this.transferOnScroll}
+							/>
+						</Route>
+						<Route
+							exact
+							path={"/"}
+						>
+							<HomePage
+								isMenuOpen={this.state.isMenuOpen}
+								toggleMenu={this.toggleMenu}
+								closeMenu={this.closeMenu}
+								applyHideClass={this.applyHideClass}
+								transferOnScroll={this.transferOnScroll}
+							/>
+						</Route>
+						<Route path="*">
+							<Error404 />
+						</Route>
+					</Switch>
 				</Suspense>
-			</>
+				<Footer />
+			</BrowserRouter>
 		);
 	}
 }
