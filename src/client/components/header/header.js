f01cf75b39a69f6e01a204200f06d12f3230c2e4
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-09-15 12:03:45 +0200

    [refactor] Call the Icon component and it props to define its look

--- src/client/components/header/header.js
+++ src/client/components/header/header.js
@@ -6,6 +6,7 @@ import React, {Component} from "react";
 
 import Name from "../name/name";
 import Button from "../button/button";
+import Icon from "../icon/icon";
 
 class Header extends Component {
 	render() {
@@ -24,7 +25,40 @@ class Header extends Component {
 						: "Ouvrir le menu"
 					}
 					class={"button__menu"}
-				/>
+				>
+					<Icon
+						topBarY={"8"}
+						topBarWidth={"36"}
+						topBarHeight={"2"}
+						topBarRx={"1"}
+						topBarFill={"white"}
+						topBarClass={
+							this.props.isMenuOpen
+							? "topBar__hamburger--close"
+							: "topBar__hamburger"
+						}
+						middleBarY={"17"}
+						middleBarWidth={"36"}
+						middleBarHeight={"2"}
+						middleBarRx={"1"}
+						middleBarFill={"white"}
+						middleBarClass={
+							this.props.isMenuOpen
+							? "middleBar__hamburger--close"
+							: "middleBar__hamburger"
+						}
+						bottomBarY={"26"}
+						bottomBarWidth={"36"}
+						bottomBarHeight={"2"}
+						bottomBarRx={"1"}
+						bottomBarFill={"white"}
+						bottomBarClass={
+							this.props.isMenuOpen
+							? "bottomBar__hamburger--close"
+							: "bottomBar__hamburger"
+						}
+					/>
+				</Button>
 			</header>
 		);
 	}
