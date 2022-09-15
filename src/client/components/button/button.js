11566335188734bbb2e2ec91f26d777e86bff802
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-09-15 12:02:47 +0200

    [refactor] Call the Icon from parent component for better flexibility

--- src/client/components/button/button.js
+++ src/client/components/button/button.js
@@ -4,8 +4,6 @@
 
 import React, {Component} from "react";
 
-import Icon from "../icon/icon";
-
 class Button extends Component {
 	render() {
 		return (
@@ -15,24 +13,7 @@ class Button extends Component {
 				alt={this.props.alt}
 				className={this.props.class}
 			>
-				<Icon
-					isMenuOpen={this.props.isMenuOpen}
-					topBarClass={
-						this.props.isMenuOpen
-						? "topBar__hamburger--close"
-						: "topBar__hamburger"
-					}
-					middleBarClass={
-						this.props.isMenuOpen
-						? "middleBar__hamburger--close"
-						: "middleBar__hamburger"
-					}
-					bottomBarClass={
-						this.props.isMenuOpen
-						? "bottomBar__hamburger--close"
-						: "bottomBar__hamburger"
-					}
-				/>
+				{this.props.children}
 			</button>
 		);
 	}
