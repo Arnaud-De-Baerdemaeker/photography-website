b1a796c10bd53722dda3570a229b223c49c843b2
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-10-22 18:59:17 +0200

    [refactor] Reunite all SVGs in one component using children props

--- src/client/components/svg/svg.js
+++ src/client/components/svg/svg.js
@@ -10,8 +10,10 @@ class SVG extends Component {
 			<svg
 				width={this.props.width}
 				height={this.props.height}
+				viewBox={this.props.viewBox}
 				className={this.props.class}
 				xmlns={"http://www.w3.org/2000/svg"}
+				xmlnsXlink={"http://www.w3.org/1999/xlink"}
 			>
 				{this.props.children}
 			</svg>
