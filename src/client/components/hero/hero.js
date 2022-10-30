41d1154241acac524abfec7b00d0218d628fcbe8
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-10-30 12:44:01 +0100

    [refactor] Set a condition to check if the props exists

--- src/client/components/hero/hero.js
+++ src/client/components/hero/hero.js
@@ -48,7 +48,11 @@ class Hero extends Component {
 	render() {
 		return (
 			<div className={"hero"}>
-				<div className={this.props.heroContainerClass}>
+				<div className={"hero__container" + (
+					this.props.heroContainerClass
+					? this.props.heroContainerClass
+					: ""
+				)}>
 					<div className={"hero__backFilter"}></div>
 				</div>
 				<h2 className={this.props.heroTitleClass}>
