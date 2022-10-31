2f247cfd003565b639cc2e062dc52b8ff38c5a81
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-10-31 16:21:58 +0100

    [refactor] Adapt the class names

--- src/client/components/header/header.js
+++ src/client/components/header/header.js
@@ -58,24 +58,24 @@ class Header extends Component {
 									d={"M35 8H1C0.447715 8 0 8.44772 0 9C0 9.55228 0.447716 10 1 10H35C35.5523 10 36 9.55228 36 9C36 8.44772 35.5523 8 35 8Z"}
 									className={
 										this.props.isMenuOpen
-										? "topFigure__hamburger--close"
-										: "topFigure__hamburger"
+										? "icon__hamburgerTop--close"
+										: "icon__hamburgerTop"
 									}
 								/>
 								<path
 									d={"M35 17H1C0.447715 17 0 17.4477 0 18C0 18.5523 0.447716 19 1 19H35C35.5523 19 36 18.5523 36 18C36 17.4477 35.5523 17 35 17Z"}
 									className={
 										this.props.isMenuOpen
-										? "middleFigure__hamburger--close"
-										: "middleFigure__hamburger"
+										? "icon__hamburgerMiddle--close"
+										: "icon__hamburgerMiddle"
 									}
 								/>
 								<path
 									d={"M35 26H1C0.447715 26 0 26.4477 0 27C0 27.5523 0.447716 28 1 28H35C35.5523 28 36 27.5523 36 27C36 26.4477 35.5523 26 35 26Z"}
 									className={
 										this.props.isMenuOpen
-										? "bottomFigure__hamburger--close"
-										: "bottomFigure__hamburger"
+										? "icon__hamburgerBottom--close"
+										: "icon__hamburgerBottom"
 									}
 								/>
 							</g>
