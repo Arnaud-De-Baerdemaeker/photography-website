ceca71cfa677750647adef54ef7780afde058191
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-11-03 16:45:24 +0100

    [refactor] Call children props

--- src/client/components/name/name.js
+++ src/client/components/name/name.js
@@ -3,21 +3,12 @@
 // By Arnaud De Baerdemaeker
 
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
 
 class Name extends Component {
 	render() {
 		return (
 			<h1 className={this.props.class}>
-				<Link
-					to={"/"}
-					onClick={this.props.closeMenu}
-					className={"name__link"}
-				>
-					{"Arnaud"}
-					<br />
-					{"De Baerdemaeker"}
-				</Link>
+				{this.props.children}
 			</h1>
 		);
 	}
