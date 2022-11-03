7b4ec83e240af8095a96bb9e482961bcf0b19757
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-11-03 16:04:12 +0100

    [refactor] Apply modifications on the component's mechanics
    - Simplified the process to initialy hide the content and revealing the content on scroll
    - Remove the intro following the title in the hero
    - Move the Modal component and its state and function here
    - Set a function to get the data from the clicked element

--- src/client/components/galleryPage/gallery.js
+++ src/client/components/galleryPage/gallery.js
@@ -7,6 +7,7 @@ import React, {Component} from "react";
 import Hero from "../hero/hero";
 import SVG from "../svg/svg";
 import PhotosCards from "../photosCards/photosCards";
+import Modal from "../modal/modal";
 
 import photos from "../../json/metadata.json";
 
@@ -14,8 +15,31 @@ class Gallery extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			photos: []
+			photos: photos,
+			isModalOpen: false
 		}
+
+		this.hdPictureFromClick;
+
+		this.toggleModal = this.toggleModal.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.getDataFromTarget = this.getDataFromTarget.bind(this);
+	}
+
+	toggleModal() {
+		this.setState(state => ({
+			isModalOpen: !state.isModalOpen
+		}));
+	}
+
+	handleClick(click) {
+		this.toggleModal();
+		this.getDataFromTarget(click);
+	}
+
+	getDataFromTarget(click) {
+		click.preventDefault();
+		this.hdPictureFromClick = click.target.dataset.hd;
 	}
 
 	componentDidMount() {
@@ -27,19 +51,21 @@ class Gallery extends Component {
 			window.scrollTo(0, 0);
 		}
 
-		// Apply a class to initially hide the content
-		this.props.applyHideClass();
+		// Get the elements to hide
+		const fetchedElements = document.querySelectorAll(".gallery__listItem");
 
-		window.addEventListener("scroll", this.props.transferOnScroll);
+		// Apply a class to initially hide the elements
+		this.props.applyHideClass(fetchedElements);
 
-		// Put the data from the json file in a state
-		this.setState({
-			photos: photos
+		// Each time the user scrolls, the list of elements is refreshed and sent to a function
+		window.addEventListener("scroll", () => {
+			const refetchedElements = fetchedElements;
+			this.props.revealOnScroll(refetchedElements);
 		});
 	}
 
 	componentWillUnmount() {
-		window.removeEventListener("scroll", this.props.transferOnScroll);
+		window.removeEventListener("scroll", () => {});
 	}
 
 	render() {
@@ -49,35 +75,32 @@ class Gallery extends Component {
 					heroContainerClass={" hero__background--1"}
 					heroTitleClass={"hero__title--gallery"}
 					heroTitleContent={
-						<>
-							<span className={"title__page"}>{"Galerie"}</span>
-							<span className={"title__intro"}>
-								{"Découvrez une sélection"}
-								<br/>
-								{"de mes captures"}
-							</span>
-						</>
+						<span className={"title__page"}>{"Galerie"}</span>
 					}
-					svgContent={
-						<SVG
-							viewBox={"0 0 36 36"}
-							class={"svg__scrollDown"}
-						>
-							<g>
-								<path
-									d={"M6.20711 2.20711L17.2929 13.2929C17.6834 13.6834 18.3166 13.6834 18.7071 13.2929L29.7929 2.20711C30.1834 1.81658 30.1834 1.18342 29.7929 0.792893L29.7071 0.707107C29.3166 0.316582 28.6834 0.316583 28.2929 0.707107L18.7071 10.2929C18.3166 10.6834 17.6834 10.6834 17.2929 10.2929L7.70711 0.707107C7.31658 0.316583 6.68342 0.316583 6.29289 0.707107L6.20711 0.792893C5.81658 1.18342 5.81658 1.81658 6.20711 2.20711Z"}
-									className={"icon__chevronTop"}
-								/>
-								<path
-									d={"M17.2929 24.2929L6.20711 13.2071C5.81658 12.8166 5.81658 12.1834 6.20711 11.7929L6.29289 11.7071C6.68342 11.3166 7.31658 11.3166 7.70711 11.7071L17.2929 21.2929C17.6834 21.6834 18.3166 21.6834 18.7071 21.2929L28.2929 11.7071C28.6834 11.3166 29.3166 11.3166 29.7071 11.7071L29.7929 11.7929C30.1834 12.1834 30.1834 12.8166 29.7929 13.2071L18.7071 24.2929C18.3166 24.6834 17.6834 24.6834 17.2929 24.2929Z"}
-									className={"icon__chevronMiddle"}
-								/>
-								<path
-									d={"M29.7929 24.2071L18.7071 35.2929C18.3166 35.6834 17.6834 35.6834 17.2929 35.2929L6.20711 24.2071C5.81658 23.8166 5.81658 23.1834 6.20711 22.7929L6.29289 22.7071C6.68342 22.3166 7.31658 22.3166 7.70711 22.7071L17.2929 32.2929C17.6834 32.6834 18.3166 32.6834 18.7071 32.2929L28.2929 22.7071C28.6834 22.3166 29.3166 22.3166 29.7071 22.7071L29.7929 22.7929C30.1834 23.1834 30.1834 23.8166 29.7929 24.2071Z"}
-									className={"icon__chevronBottom"}
-								/>
-							</g>
-						</SVG>
+					scrollDownSVG={
+						<div className={"hero__scrollDown"}>
+							<div className="hero__scrollDownContainer">
+								<SVG
+									viewBox={"0 0 36 36"}
+									class={"svg__scrollDown"}
+								>
+									<g>
+										<path
+											d={"M6.20711 2.20711L17.2929 13.2929C17.6834 13.6834 18.3166 13.6834 18.7071 13.2929L29.7929 2.20711C30.1834 1.81658 30.1834 1.18342 29.7929 0.792893L29.7071 0.707107C29.3166 0.316582 28.6834 0.316583 28.2929 0.707107L18.7071 10.2929C18.3166 10.6834 17.6834 10.6834 17.2929 10.2929L7.70711 0.707107C7.31658 0.316583 6.68342 0.316583 6.29289 0.707107L6.20711 0.792893C5.81658 1.18342 5.81658 1.81658 6.20711 2.20711Z"}
+											className={"icon__chevronTop"}
+										/>
+										<path
+											d={"M17.2929 24.2929L6.20711 13.2071C5.81658 12.8166 5.81658 12.1834 6.20711 11.7929L6.29289 11.7071C6.68342 11.3166 7.31658 11.3166 7.70711 11.7071L17.2929 21.2929C17.6834 21.6834 18.3166 21.6834 18.7071 21.2929L28.2929 11.7071C28.6834 11.3166 29.3166 11.3166 29.7071 11.7071L29.7929 11.7929C30.1834 12.1834 30.1834 12.8166 29.7929 13.2071L18.7071 24.2929C18.3166 24.6834 17.6834 24.6834 17.2929 24.2929Z"}
+											className={"icon__chevronMiddle"}
+										/>
+										<path
+											d={"M29.7929 24.2071L18.7071 35.2929C18.3166 35.6834 17.6834 35.6834 17.2929 35.2929L6.20711 24.2071C5.81658 23.8166 5.81658 23.1834 6.20711 22.7929L6.29289 22.7071C6.68342 22.3166 7.31658 22.3166 7.70711 22.7071L17.2929 32.2929C17.6834 32.6834 18.3166 32.6834 18.7071 32.2929L28.2929 22.7071C28.6834 22.3166 29.3166 22.3166 29.7071 22.7071L29.7929 22.7929C30.1834 23.1834 30.1834 23.8166 29.7929 24.2071Z"}
+											className={"icon__chevronBottom"}
+										/>
+									</g>
+								</SVG>
+							</div>
+						</div>
 					}
 				/>
 				<main className={"gallery"}>
@@ -91,10 +114,18 @@ class Gallery extends Component {
 									sd={photo.sd}
 									hd={photo.hd}
 									location={photo.location}
+									isModalOpen={this.state.isModalOpen}
+									toggleModal={this.toggleModal}
+									handleClick={this.handleClick}
 								/>
 							</li>
 						)}
 					</ul>
+					<Modal
+						hd={this.hdPictureFromClick}
+						isModalOpen={this.state.isModalOpen}
+						toggleModal={this.toggleModal}
+					/>
 				</main>
 			</>
 		);
