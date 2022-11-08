c09466a26d4e1ac9d4962f8ec9e9ddc084f9a61d
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-11-08 16:49:06 +0100

    [refactor] Rework the component
    - Use ref to target an element
    - Remove the innerContainer of the header

--- src/client/components/header/header.js
+++ src/client/components/header/header.js
@@ -2,7 +2,7 @@
 // Started on July 2020
 // By Arnaud De Baerdemaeker
 
-import React, {Component} from "react";
+import React, {Component, createRef} from "react";
 import {Link} from "react-router-dom";
 
 import Name from "../name/name";
@@ -10,15 +10,19 @@ import Button from "../button/button";
 import SVG from "../svg/svg";
 
 class Header extends Component {
-	componentDidMount() {
-		let headerElement = document.querySelector("#innerHeader");
+	constructor(props) {
+		super(props);
 
+		this.header = createRef();
+	}
+
+	componentDidMount() {
 		window.addEventListener("scroll", () => {
 			if(window.scrollY > 0) {
-				headerElement.classList.replace("header__innerContainer", "header__innerContainer--scroll");
+				this.header.current.classList.add("scroll");
 			}
 			else if (window.screenY < 1) {
-				headerElement.classList.replace("header__innerContainer--scroll", "header__innerContainer");
+				this.header.current.classList.remove("scroll");
 			}
 		});
 	}
@@ -30,69 +34,61 @@ class Header extends Component {
 	render() {
 		return (
 			<header
-				id={"header"}
+				ref={this.header}
 				className={"header"}
 			>
-				<div
-					id={"innerHeader"}
-					className={"header__innerContainer"}
+				<Name class={"name--header"}>
+					<Link
+						to={"/"}
+						onClick={this.props.closeMenu}
+						className={"name__link"}
+					>
+						{"Arnaud"}
+						<br />
+						{"De Baerdemaeker"}
+					</Link>
+				</Name>
+				<Button
+					alt={
+						this.props.isMenuOpen
+						? "Fermer le menu"
+						: "Ouvrir le menu"
+					}
+					function={this.props.toggleMenu}
+					class={"button__menu"}
 				>
-					<Name
-						class={"name--header"}
-						closeMenu={this.props.closeMenu}
+					<SVG
+						viewBox={"0 0 36 36"}
+						class={"svg__menu"}
 					>
-						<Link
-							to={"/"}
-							onClick={this.props.closeMenu}
-							className={"name__link"}
-						>
-							{"Arnaud"}
-							<br />
-							{"De Baerdemaeker"}
-						</Link>
-					</Name>
-					<Button
-						alt={
-							this.props.isMenuOpen
-							? "Fermer le menu"
-							: "Ouvrir le menu"
-						}
-						function={this.props.toggleMenu}
-						class={"button__menu"}
-					>
-						<SVG
-							viewBox={"0 0 36 36"}
-							class={"svg__menu"}
-						>
-							<g>
-								<path
-									d={"M35 8H1C0.447715 8 0 8.44772 0 9C0 9.55228 0.447716 10 1 10H35C35.5523 10 36 9.55228 36 9C36 8.44772 35.5523 8 35 8Z"}
-									className={
-										this.props.isMenuOpen
-										? "icon__hamburgerTop--close"
-										: "icon__hamburgerTop"
-									}
-								/>
-								<path
-									d={"M35 17H1C0.447715 17 0 17.4477 0 18C0 18.5523 0.447716 19 1 19H35C35.5523 19 36 18.5523 36 18C36 17.4477 35.5523 17 35 17Z"}
-									className={
-										this.props.isMenuOpen
-										? "icon__hamburgerMiddle--close"
-										: "icon__hamburgerMiddle"
-									}
-								/>
-								<path
-									d={"M35 26H1C0.447715 26 0 26.4477 0 27C0 27.5523 0.447716 28 1 28H35C35.5523 28 36 27.5523 36 27C36 26.4477 35.5523 26 35 26Z"}
-									className={
-										this.props.isMenuOpen
-										? "icon__hamburgerBottom--close"
-										: "icon__hamburgerBottom"
-									}
-								/>
-							</g>
-						</SVG>
-					</Button>
-				</div>
+						<g>
+							<path
+								d={"M35 8H1C0.447715 8 0 8.44772 0 9C0 9.55228 0.447716 10 1 10H35C35.5523 10 36 9.55228 36 9C36 8.44772 35.5523 8 35 8Z"}
+								className={
+									this.props.isMenuOpen
+									? "icon__hamburgerTop--close"
+									: "icon__hamburgerTop"
+								}
+							/>
+							<path
+								d={"M35 17H1C0.447715 17 0 17.4477 0 18C0 18.5523 0.447716 19 1 19H35C35.5523 19 36 18.5523 36 18C36 17.4477 35.5523 17 35 17Z"}
+								className={
+									this.props.isMenuOpen
+									? "icon__hamburgerMiddle--close"
+									: "icon__hamburgerMiddle"
+								}
+							/>
+							<path
+								d={"M35 26H1C0.447715 26 0 26.4477 0 27C0 27.5523 0.447716 28 1 28H35C35.5523 28 36 27.5523 36 27C36 26.4477 35.5523 26 35 26Z"}
+								className={
+									this.props.isMenuOpen
+									? "icon__hamburgerBottom--close"
+									: "icon__hamburgerBottom"
+								}
+							/>
+						</g>
+					</SVG>
+				</Button>
 			</header>
 		);
 	}
