3cf722de4366f8e67add7008f801b1e497b1fe88
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-02-16 16:55:45 +0100

    [refactor] Add the link to the websites

--- src/client/components/project/project.js
+++ src/client/components/project/project.js
@@ -8,13 +8,19 @@ class Project extends Component {
 	render() {
 		return(
 			<section className={"project"}>
-				<img
-					src={this.props.projectImageUrl}
-					alt={this.props.projectImageAlt}
-					className={"project__image"}
-				/>
-				<h2 className={"project__title"}>{this.props.projectTitle}</h2>
-				<p className={"project__description"}>{this.props.projectDescription}</p>
+				<a
+					href={this.props.projectLink}
+					target={"_blank"}
+					rel={"noreferrer noopener"}
+					className={"work"}
+				>
+					<img
+						src={this.props.projectImageUrl}
+						alt={this.props.projectImageAlt}
+						className={"work__image"}
+					/>
+					<h2 className={"work__title"}>{this.props.projectTitle}</h2>
+				</a>
 			</section>
 		);
 	}
